5b228ff67787c2db9baa9605a57bcd4ff3affd9b
Alex Appleget <alexappleget2014@gmail.com>
2024-12-03 11:15:29 -0600

    Feat: made it so that we can pass in any string into the avatar component for the photo. will work well for when user's want to upload their own

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -2,10 +2,10 @@ import { AvatarImage } from "./AvatarImage";
 import { AvatarFallback } from "./AvatarFallback";
 import { AvatarBody } from "./AvatarBody";
 
-const Avatar = () => {
+const Avatar = ({ userAvatar }: { userAvatar: string }) => {
   return (
     <AvatarBody>
-      <AvatarImage src="" alt="" />
+      <AvatarImage src={userAvatar} alt="" />
       <AvatarFallback>CN</AvatarFallback>
     </AvatarBody>
   );
--- components/GlobalHeader/GlobalHeader.tsx
+++ components/GlobalHeader/GlobalHeader.tsx
@@ -4,7 +4,7 @@ const GlobalHeader = () => {
   return (
     <nav className="flex items-center justify-between px-52 border-2 border-black h-20">
       <div className="border-2 border-blue-500"></div>
-      <Avatar />
+      <Avatar userAvatar="" />
     </nav>
   );
 };
