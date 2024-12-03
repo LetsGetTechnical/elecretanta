b29a9275a493bb90c24b4c5b9f6ca259c3a9e8f5
Alex Appleget <alexappleget2014@gmail.com>
2024-12-03 16:21:51 -0600

    Feat: added the border header green to tailwind config and building out nav logo

--- components/GlobalHeader/GlobalHeader.tsx
+++ components/GlobalHeader/GlobalHeader.tsx
@@ -1,9 +1,10 @@
 import Avatar from "../Avatar/Avatar";
+import NavLogo from "../NavLogo/NavLogo";
 
 const GlobalHeader = () => {
   return (
-    <nav className="flex items-center justify-between px-52 border-2 border-black h-20">
-      <div className="border-2 border-blue-500"></div>
+    <nav className="flex items-center justify-between px-52 bg-elfGreen h-20">
+      <NavLogo />
       <Avatar userAvatar="https://static.vecteezy.com/system/resources/previews/024/183/525/non_2x/avatar-of-a-man-portrait-of-a-young-guy-illustration-of-male-character-in-modern-color-style-vector.jpg" />
     </nav>
   );
---
+++ components/NavLogo/NavLogo.tsx
@@ -0,0 +1,12 @@
+import { Gift } from "lucide-react";
+
+const NavLogo = () => {
+  return (
+    <div>
+      <Gift className="bg-" />
+      <p></p>
+    </div>
+  );
+};
+
+export default NavLogo;
