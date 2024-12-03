9ad708e433d9ad9986b0d8cec69c2879589d2562
Alex Appleget <alexappleget2014@gmail.com>
2024-12-03 11:19:14 -0600

    Fix: Added example avatar logo for a filler for now.

--- components/GlobalHeader/GlobalHeader.tsx
+++ components/GlobalHeader/GlobalHeader.tsx
@@ -4,7 +4,7 @@ const GlobalHeader = () => {
   return (
     <nav className="flex items-center justify-between px-52 border-2 border-black h-20">
       <div className="border-2 border-blue-500"></div>
-      <Avatar userAvatar="" />
+      <Avatar userAvatar="https://static.vecteezy.com/system/resources/previews/024/183/525/non_2x/avatar-of-a-man-portrait-of-a-young-guy-illustration-of-male-character-in-modern-color-style-vector.jpg" />
     </nav>
   );
 };
