cab41f04bfd90de4a2a5cd20a238843335301a05
Cody Epstein <codyepstein@gmail.com>
2024-12-06 12:18:59 -0800

    fix supabase client imports

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,20 +1,20 @@
-import { createClient } from "@/lib/utils/supabase/server";
+import { createClient } from "@/lib/supabase/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get("code");
-  const redirectPath =
-    requestUrl.searchParams.get("redirect_to") || "/onboarding";
+	const requestUrl = new URL(request.url);
+	const code = requestUrl.searchParams.get("code");
+	const redirectPath =
+		requestUrl.searchParams.get("redirect_to") || "/onboarding";
 
-  if (code) {
-    const supabase = await createClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
+	if (code) {
+		const supabase = await createClient();
+		const { error } = await supabase.auth.exchangeCodeForSession(code);
 
-    if (!error) {
-      return NextResponse.redirect(`${requestUrl.origin}${redirectPath}`);
-    }
-  }
+		if (!error) {
+			return NextResponse.redirect(`${requestUrl.origin}${redirectPath}`);
+		}
+	}
 
-  return NextResponse.redirect(`${requestUrl.origin}/auth-code-error`);
+	return NextResponse.redirect(`${requestUrl.origin}/auth-code-error`);
 }
