2213c0d4af954b642ed083327aa248046c323308
Jennifer Tieu <jennifer.tieuu@gmail.com>
2024-12-12 10:16:01 -0600

    feat: set max width on card wrapper

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,7 +3,9 @@ import ProfileCard from "@/components/ProfileCard/ProfileCard";
 export default function PersonalProfile() {
   return (
     <section className="h-svh flex justify-center items-start py-16">
-      <ProfileCard showEditButton />
+      <div className="max-w-5xl">
+        <ProfileCard showEditButton />
+      </div>
     </section>
   );
 }
