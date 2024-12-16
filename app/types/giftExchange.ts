3ecf542ce215358703d2b43e7d4c93bf21db810e
Cody Epstein <codyepstein@gmail.com>
2024-12-16 08:19:37 -0800

    modify giftExchange type

--- app/types/giftExchange.ts
+++ app/types/giftExchange.ts
@@ -1,30 +1,30 @@
 export interface GiftExchange {
-  id: string;
-  name: string;
-  description?: string;
-  group_image: string;
-  budget: number;
-  drawing_date: Date;
-  exchange_date: Date;
-  owner_id: string;
-  created_at?: Date;
-  updated_at?: Date;
+	id: string;
+	name: string;
+	description?: string;
+	group_image: string;
+	budget: number;
+	drawing_date: string;
+	exchange_date: string;
+	owner_id: string;
+	created_at?: Date;
+	updated_at?: Date;
 }
 
 export interface CreateGiftExchangeRequest {
-  name: string;
-  description?: string;
-  group_image: string;
-  budget: number;
-  drawing_date: string;
-  exchange_date: string;
+	name: string;
+	description?: string;
+	group_image: string;
+	budget: number;
+	drawing_date: string;
+	exchange_date: string;
 }
 
 export interface UpdateGiftExchangeRequest {
-  name?: string;
-  description?: string;
-  group_image?: string;
-  budget?: number;
-  drawing_date?: string;
-  exchange_date?: string;
+	name?: string;
+	description?: string;
+	group_image?: string;
+	budget?: number;
+	drawing_date?: string;
+	exchange_date?: string;
 }
