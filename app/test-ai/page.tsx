3963b482c13efeff376e75a7a6343e9e468ef59c
Alex Appleget <alexappleget2014@gmail.com>
2024-12-18 10:52:07 -0600

    Fix: commented out the ai function

--- app/test-ai/page.tsx
+++ app/test-ai/page.tsx
@@ -13,20 +13,20 @@ export interface Gift {
 
 const TestAIPage = (): React.JSX.Element => {
   const [gifts, setGifts] = useState<Record<string, Gift>>({});
-  const fetchGifts = async () => {
-    const response = await fetch("/api/giftSuggestions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({}),
-    });
+  // const fetchGifts = async () => {
+  //   const response = await fetch("/api/giftSuggestions", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({}),
+  //   });
 
-    const data = await response.json();
-    const dataObject = JSON.parse(data.message.content);
-    console.log(dataObject);
-    setGifts(dataObject);
-  };
+  //   const data = await response.json();
+  //   const dataObject = JSON.parse(data.message.content);
+  //   console.log(dataObject);
+  //   setGifts(dataObject);
+  // };
 
   // useEffect(() => {
   //   fetchGifts();
