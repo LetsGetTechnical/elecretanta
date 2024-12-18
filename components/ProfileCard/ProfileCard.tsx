018c04385c6925c441b8e1ce94f88a86b1b6b9e4
Jennifer Tieu <jennifer.tieuu@gmail.com>
2024-12-18 10:41:32 -0600

    feat: populate profile

--- components/ProfileCard/ProfileCard.tsx
+++ components/ProfileCard/ProfileCard.tsx
@@ -27,16 +27,60 @@ const ProfileCard = ({ showEditButton = false }: ProfileCardProps) => {
     fetchProfile();
   }, []);
 
+  const formatCategory = (category: string) => {
+    const categories = category.split("&");
+    const formattedCategories = categories.map((categoryItem) => {
+      const trimmedCategoryItem = categoryItem.trim();
+      const firstLetter = trimmedCategoryItem.charAt(0).toUpperCase();
+      const restOfCategory = trimmedCategoryItem.slice(1);
+      return `${firstLetter}${restOfCategory}`;
+    });
+    return formattedCategories.join(" & ");
+  };
+
+  const renderPreference = (
+    preferenceRight: string,
+    preferenceLeft: string,
+    preference_value: number | undefined
+  ) => {
+    if (!preference_value) {
+      return (
+        <div className="flex flex-col gap-2 text-xs">
+          <div className="flex items-center justify-between">
+            <div>{preferenceLeft}</div>
+            <div>{preferenceRight}</div>
+          </div>
+          <Progress indicatorClassName="bg-[#E8577D]" value={0} />
+        </div>
+      );
+    }
+    return (
+      <div className="flex flex-col gap-2 text-xs">
+        <div className="flex items-center justify-between">
+          <div className={preference_value <= 50 ? "font-bold" : ""}>
+            {preferenceLeft}
+          </div>
+          <div className={preference_value >= 50 ? "font-bold" : ""}>
+            {preferenceRight}
+          </div>
+        </div>
+        <Progress indicatorClassName="bg-[#E8577D]" value={preference_value} />
+      </div>
+    );
+  };
+
   return (
     <article className="text-white px-4">
       <div className="flex items-center rounded-t-2xl justify-between py-5 px-9 bg-groupCardGreen">
         <div className="flex items-center gap-4">
           <Avatar userAvatar="https://static.vecteezy.com/system/resources/previews/024/183/525/non_2x/avatar-of-a-man-portrait-of-a-young-guy-illustration-of-male-character-in-modern-color-style-vector.jpg" />
           <div>
-            <h1 className="text-lg font-bold">John Doe</h1>
+            <h1 className="text-lg font-bold">{profile?.display_name}</h1>
             <div className="flex items-center gap-2 text-sm font-medium">
               <Cake className="text-[#92AEA9]" size={16} />
-              <span className="text-sm text-[#EDE6DF]">25-34</span>
+              <span className="text-sm text-[#EDE6DF]">
+                {profile?.age_group}
+              </span>
             </div>
           </div>
         </div>
@@ -53,54 +97,38 @@ const ProfileCard = ({ showEditButton = false }: ProfileCardProps) => {
               <Heart size={16} strokeWidth={2} /> Interests & Hobbies
             </h2>
             <div className="flex flex-wrap gap-2">
-              <Badge className="text-sm py-1 px-2.5 rounded-xl bg-[#DBDBB2] text-[#292913] hover:text-white">
-                Books & Stories
-              </Badge>
-              <Badge className="text-sm py-1 px-2.5 rounded-xl bg-[#DBDBB2] text-[#292913] hover:text-white">
-                Art & Creation
-              </Badge>
+              {profile?.categories.map((category) => {
+                const categoryName = formatCategory(category);
+                return (
+                  <Badge
+                    key={category}
+                    className="text-sm py-1 px-2.5 rounded-xl bg-[#DBDBB2] text-[#292913] hover:text-white"
+                  >
+                    {categoryName}
+                  </Badge>
+                );
+              })}
             </div>
-            <p className="text-sm">
-              Love reading sci-fi novels, collecting mechanical keyboards, and
-              painting digital art
-            </p>
+            <p className="text-sm">{profile?.hobbies}</p>
           </div>
           <div className="flex flex-col gap-2">
             <h2 className="flex items-center gap-3 font-bold text-[#FDDEDE]">
               <CircleX size={16} strokeWidth={2} /> Please avoid
             </h2>
-            <p className="text-sm">
-              I have a sensitivity to scented products and am trying to minimize
-              plastic items - also not a big fan of super spicy foods (allergic
-              to cayenne pepper).
-            </p>
+            <p className="text-sm">{profile?.avoid}</p>
           </div>
         </div>
         <div className="flex flex-col gap-5">
           <h2 className="flex items-center gap-2 font-bold mb-2 text-[#E8577D]">
             <Compass size={16} strokeWidth={2} /> Gift Styles
           </h2>
-          <div className="flex flex-col gap-2 text-xs">
-            <div className="flex items-center justify-between">
-              <div>Practical</div>
-              <div className="font-bold">Whimsical</div>
-            </div>
-            <Progress indicatorClassName="bg-[#E8577D]" value={95} />
-          </div>
-          <div className="flex flex-col gap-2 text-xs">
-            <div className="flex items-center justify-between">
-              <div className="font-bold">Cozy</div>
-              <div>Adventurous</div>
-            </div>
-            <Progress indicatorClassName="bg-[#E8577D]" value={15} />
-          </div>
-          <div className="flex flex-col gap-2 text-xs">
-            <div className="flex items-center justify-between">
-              <div>Minimal</div>
-              <div className="font-bold">Luxurious</div>
-            </div>
-            <Progress indicatorClassName="bg-[#E8577D]" value={65} />
-          </div>
+          {renderPreference(
+            "Practical",
+            "Whimsical",
+            profile?.practical_whimsical
+          )}
+          {renderPreference("Cozy", "Adventurous", profile?.cozy_adventurous)}
+          {renderPreference("Minimal", "Luxurious", profile?.minimal_luxurious)}
         </div>
       </div>
     </article>
