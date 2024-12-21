a25d046139c9e7928ccae5dcd9508cd354ce044b
Cody Epstein <codyepstein@gmail.com>
2024-12-20 22:22:37 -0800

    fix stlying and add Link to group page

--- components/GroupCard/GroupCard.tsx
+++ components/GroupCard/GroupCard.tsx
@@ -1,15 +1,16 @@
-import { GiftExchange } from "@/app/types/giftExchange";
-import { ChevronRight } from "lucide-react";
+import { GiftExchangeWithMemberCount } from "@/app/types/giftExchange";
+import { formatDate } from "@/lib/utils";
+import { ChevronRight, Users } from "lucide-react";
 import Link from "next/link";
 
 type GroupCardProps = {
-	giftExchange: GiftExchange;
+	giftExchange: GiftExchangeWithMemberCount;
 };
 
 const GroupCard = ({ giftExchange }: GroupCardProps) => {
 	return (
-		<Link href="">
-			<div className="h-28 lg:w-1/2 flex items-center p-4 rounded-xl bg-groupCardGreen">
+		<Link href={`/gift-exchanges/${giftExchange.gift_exchange_id}`}>
+			<div className="h-28 flex items-center p-4 rounded-xl bg-groupCardGreen">
 				<img
 					className="h-16 w-16 lg:h-20 lg:w-20 rounded-xl"
 					src={giftExchange.group_image}
@@ -19,10 +20,16 @@ const GroupCard = ({ giftExchange }: GroupCardProps) => {
 					<h2 className="font-semibold text-white text-base lg:text-lg">
 						{giftExchange.name}
 					</h2>
-					<div className="flex gap-4">
-						<p className="text-white text-xs lg:text-sm">5 members</p>
+					<div className="flex gap-4 items-center">
+						<span className="flex gap-2 items-center">
+							<Users color="white" opacity={0.7} />
+							<p className="text-white text-xs lg:text-sm">
+								{giftExchange.member_count}{" "}
+								{giftExchange.member_count === 1 ? "member" : "members"}
+							</p>
+						</span>
 						<p className="text-white text-xs lg:text-sm">
-							Draw: {giftExchange.drawing_date}
+							Draw: {formatDate(giftExchange.drawing_date)}
 						</p>
 					</div>
 				</div>
