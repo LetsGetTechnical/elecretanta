55a8e9e613bcef57f3ef7a2b60de53260edc0cc1
Cody Epstein <codyepstein@gmail.com>
2024-12-25 16:09:10 -0800

    add react-snow-overlay

---
+++ components/SnowOverlayWrapper/react-snow-overlay.d.ts
@@ -0,0 +1,7 @@
+declare module "react-snow-overlay" {
+	export interface SnowOverlayProps {
+		disabled?: boolean;
+		disabledOnSingleCpuDevices?: boolean;
+	}
+	export const SnowOverlay: React.FC<SnowOverlayProps>;
+}
