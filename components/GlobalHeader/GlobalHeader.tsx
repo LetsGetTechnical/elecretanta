6b0d5f784a631cb9e427b03fcb32e61dbb86489b
Alex Appleget <alexappleget2014@gmail.com>
2024-12-26 13:09:04 -0600

    Fix: set up global header to be able to fetch and display the avatar now

--- components/GlobalHeader/GlobalHeader.tsx
+++ components/GlobalHeader/GlobalHeader.tsx
@@ -1,17 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Avatar from "../Avatar/Avatar";
 import NavLogo from "../NavLogo/NavLogo";
 import SnowOverlayToggle from "../SnowOverlayToggle/SnowOverlayToggle";
+import getUserAvatar from "@/lib/getUserAvatar";
 
 const GlobalHeader = () => {
-	return (
-		<nav className="flex items-center justify-between px-4 md:px-16 lg:px-32 xl:px-52 bg-elfHeaderGreen h-20">
-			<NavLogo />
-			<div className="flex gap-2 items-center">
-				<SnowOverlayToggle />
-				<Avatar userAvatar="https://static.vecteezy.com/system/resources/previews/024/183/525/non_2x/avatar-of-a-man-portrait-of-a-young-guy-illustration-of-male-character-in-modern-color-style-vector.jpg" />
-			</div>
-		</nav>
-	);
+  const [avatar, setAvatar] = useState<string>("");
+
+  useEffect(() => {
+    const fetchAvatar = async () => {
+      const response = await getUserAvatar();
+      setAvatar(response);
+    };
+    fetchAvatar();
+  }, []);
+
+  return (
+    <nav className="flex items-center justify-between px-4 md:px-16 lg:px-32 xl:px-52 bg-elfHeaderGreen h-20">
+      <NavLogo />
+      <div className="flex gap-2 items-center">
+        <SnowOverlayToggle />
+        <Avatar userAvatar={avatar} />
+      </div>
+    </nav>
+  );
 };
 
 export default GlobalHeader;
