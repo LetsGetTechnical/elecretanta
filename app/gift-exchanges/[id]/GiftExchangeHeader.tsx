d3658f13e5afce46a17fb9efa00ffead6156dff0
jackmonaco20 <jackmonaco20@yahoo.com>
2024-12-29 21:07:24 -0500

    Fix: Hydration error due to nested <p> tags

--- app/gift-exchanges/[id]/GiftExchangeHeader.tsx
+++ app/gift-exchanges/[id]/GiftExchangeHeader.tsx
@@ -50,15 +50,6 @@ export const GiftExchangeHeader = ({
 
   return (
     <>
-      <div>
-        <Image
-          alt="banner image"
-          src={giftExchangeData.group_image}
-          width={1980}
-          height={1200}
-          className="w-full h-48 object-cover rounded-xl"
-        ></Image>
-      </div>
       <div className="flex justify-between">
         <Link href={"/dashboard"} className="flex items-center gap-1 text-sm">
           <ChevronLeft size={16} strokeWidth={2.25} />
@@ -74,7 +65,7 @@ export const GiftExchangeHeader = ({
         <div className="w-36 h-36 grow-0 shrink-0">
           <img
             className="w-full h-full rounded-xl ring-4 ring-white"
-            src="https://img.freepik.com/free-vector/santa-claus-elements-red-background_1057-2152.jpg"
+            src={giftExchangeData.group_image}
             alt="Group logo"
           />
         </div>
@@ -91,9 +82,9 @@ export const GiftExchangeHeader = ({
           <div className="flex sm:flex-row flex-wrap border-t pt-2 gap-4 sm:gap-16">
             <p className="flex items-center gap-2 basis-full sm:basis-auto">
               {getStatusIcon(giftExchangeData.status)}
-              <p className="text-md font-semibold">
+              <div className="text-md font-semibold">
                 {getStatusText(giftExchangeData.status)}
-              </p>
+              </div>
             </p>
             <div className="basis-full sm:basis-auto">
               <p className="text-lg font-semibold">Gift Budget</p>
