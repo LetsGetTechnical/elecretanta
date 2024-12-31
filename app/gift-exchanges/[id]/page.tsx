5af1b5e361019e2633e21997009f521d5a4aa5cf
Cody Epstein <codyepstein@gmail.com>
2024-12-31 15:49:34 -0800

    remove logic to check if owner from gift-exchange page

--- app/gift-exchanges/[id]/page.tsx
+++ app/gift-exchanges/[id]/page.tsx
@@ -22,7 +22,6 @@ export default function GiftExchangePage() {
 	const { id } = useParams();
 	const [session, setSession] = useState<Session | null>(null);
 	const [isUserAMember, setIsUserAMember] = useState<boolean | null>(null);
-	const [isOwner, setIsOwner] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
 	const [giftExchangeData, setGiftExchangeData] = useState<GiftExchange>({
 		id: "",
@@ -105,9 +104,6 @@ export default function GiftExchangePage() {
 
 	useEffect(() => {
 		fetchGiftExchangeData();
-		if (session?.user.id && giftExchangeData.owner_id) {
-			setIsOwner(session.user.id === giftExchangeData.owner_id);
-		}
 	}, [fetchGiftExchangeData, session, id]);
 
 	const updateGiftExchangeMembers = async () => {
@@ -122,54 +118,54 @@ export default function GiftExchangePage() {
 		return <LoadingSkeleton statsCount={4} cardItemCount={10} />;
 	}
 
-  const renderContent = () => {
-    switch (giftExchangeData.status) {
-      case "pending":
-        return (
-          <div className="flex flex-row w-full py-12 gap-8 items-start">
-            <JourneyCard
-              drawingDate={giftExchangeData.drawing_date}
-              exchangeDate={giftExchangeData.exchange_date}
-            />
-            <div className="flex flex-col gap-4 w-full max-w-md">
-              <MembersList members={giftExchangeMembers} />
-              <InviteCard />
-            </div>
-          </div>
-        );
-      case "active":
-        return (
-          <div className="w-full py-4">
-            <section className="py-4 mb-12">
-              <h1 className="font-bold mb-4">Your Secret Santa Match</h1>
-              <ProfileCard profile={giftMatch} />
-            </section>
-            <section className="flex flex-col">
-              <h1 className="font-bold">Gift Suggestions</h1>
-              <div className="flex flex-row flex-wrap">
-                {giftSuggestions.map((gift, index) => (
-                  <GiftSuggestionCard
-                    allGiftSuggestions={giftSuggestions}
-                    budget={giftExchangeData.budget}
-                    gift={gift}
-                    index={index}
-                    key={gift.id}
-                    onGiftUpdate={handleGiftUpdate}
-                    recipient={giftMatch}
-                  />
-                ))}
-              </div>
-            </section>
-          </div>
-        );
-      case "completed":
-        return (
-          <div className="w-full py-12">
-            <CompletedExchangeCard members={giftExchangeMembers} />
-          </div>
-        );
-    }
-  };
+	const renderContent = () => {
+		switch (giftExchangeData.status) {
+			case "pending":
+				return (
+					<div className="flex flex-row w-full py-12 gap-8 items-start">
+						<JourneyCard
+							drawingDate={giftExchangeData.drawing_date}
+							exchangeDate={giftExchangeData.exchange_date}
+						/>
+						<div className="flex flex-col gap-4 w-full max-w-md">
+							<MembersList members={giftExchangeMembers} />
+							<InviteCard />
+						</div>
+					</div>
+				);
+			case "active":
+				return (
+					<div className="w-full py-4">
+						<section className="py-4 mb-12">
+							<h1 className="font-bold mb-4">Your Secret Santa Match</h1>
+							<ProfileCard profile={giftMatch} />
+						</section>
+						<section className="flex flex-col">
+							<h1 className="font-bold">Gift Suggestions</h1>
+							<div className="flex flex-row flex-wrap">
+								{giftSuggestions.map((gift, index) => (
+									<GiftSuggestionCard
+										allGiftSuggestions={giftSuggestions}
+										budget={giftExchangeData.budget}
+										gift={gift}
+										index={index}
+										key={gift.id}
+										onGiftUpdate={handleGiftUpdate}
+										recipient={giftMatch}
+									/>
+								))}
+							</div>
+						</section>
+					</div>
+				);
+			case "completed":
+				return (
+					<div className="w-full py-12">
+						<CompletedExchangeCard members={giftExchangeMembers} />
+					</div>
+				);
+		}
+	};
 
 	return (
 		<main className="min-h-screen-minus-20">
@@ -182,10 +178,7 @@ export default function GiftExchangePage() {
 				/>
 			)}
 			<section className="mx-auto flex flex-col gap-4 px-4 md:px-16 lg:px-32 xl:px-52 pt-12 text-primary-foreground">
-				<GiftExchangeHeader
-					giftExchangeData={giftExchangeData}
-					isOwner={isOwner}
-				/>
+				<GiftExchangeHeader giftExchangeData={giftExchangeData} />
 				{renderContent()}
 			</section>
 		</main>
