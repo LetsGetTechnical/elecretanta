85b81cd45acaa2c1db41706d9e2b9608a7d4acce
Alex Appleget <alexappleget2014@gmail.com>
2024-12-31 18:48:20 -0600

    Fix: changed "/" navigation to "/dashboard" for the logo

--- components/NavLogo/NavLogo.tsx
+++ components/NavLogo/NavLogo.tsx
@@ -2,14 +2,14 @@ import { Gift } from "lucide-react";
 import Link from "next/link";
 
 const NavLogo = () => {
-	return (
-		<Link href={"/"} className="flex items-center">
-			<Gift className="bg-logoWhite h-8 w-8 p-1 rounded-lg text-red-600" />
-			<p className="font-bold ml-2 italic text-green-200">
-				Elf<span className="text-white font-semibold">gorithm</span>
-			</p>
-		</Link>
-	);
+  return (
+    <Link href={"/dashboard"} className="flex items-center">
+      <Gift className="bg-logoWhite h-8 w-8 p-1 rounded-lg text-red-600" />
+      <p className="font-bold ml-2 italic text-green-200">
+        Elf<span className="text-white font-semibold">gorithm</span>
+      </p>
+    </Link>
+  );
 };
 
 export default NavLogo;
