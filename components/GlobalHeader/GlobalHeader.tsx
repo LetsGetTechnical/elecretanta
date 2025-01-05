da466d42b5949f421332f52437c59a524ebb9bbc
Alex Appleget <alexappleget2014@gmail.com>
2025-01-05 17:31:03 -0600

    Fix: made it so the global header is hidden if the user is on the main / page and not signed in

--- components/GlobalHeader/GlobalHeader.tsx
+++ components/GlobalHeader/GlobalHeader.tsx
@@ -6,9 +6,12 @@ import NavLogo from "../NavLogo/NavLogo";
 import SnowOverlayToggle from "../SnowOverlayToggle/SnowOverlayToggle";
 import getUserAvatar from "@/lib/getUserAvatar";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const GlobalHeader = () => {
   const [avatar, setAvatar] = useState<string>("");
+  const pathname = usePathname();
 
   useEffect(() => {
     const fetchAvatar = async () => {
@@ -19,7 +22,12 @@ const GlobalHeader = () => {
   }, []);
 
   return (
-    <nav className="flex items-center justify-between px-4 md:px-16 lg:px-32 xl:px-52 bg-elfHeaderGreen h-20">
+    <nav
+      className={cn(
+        "flex items-center justify-between px-4 md:px-16 lg:px-32 xl:px-52 bg-elfHeaderGreen h-20",
+        pathname === "/" ? "hidden" : ""
+      )}
+    >
       <NavLogo />
       <div className="flex gap-2 items-center">
         <SnowOverlayToggle />
