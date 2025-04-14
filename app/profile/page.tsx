f4259ec5a74e10b25582430c2572d39d03ae8850
Cody Epstein <codyepstein@gmail.com>
2025-04-14 10:53:53 -0700

    add back to dashboard link

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,6 +3,8 @@ import ProfileCard from '@/components/ProfileCard/ProfileCard';
 import { useEffect, useState } from 'react';
 import { Profile } from '@/app/types/profile';
 import LoadingCard from '@/components/LoadingCard/LoadingCard';
+import Link from 'next/link';
+import { ChevronLeft } from 'lucide-react';
 
 export default function PersonalProfile() {
   const [profile, setProfile] = useState<Profile | null>(null);
@@ -35,7 +37,16 @@ export default function PersonalProfile() {
   }
 
   return (
-    <section className="h-svh flex justify-center items-start py-16">
+    <section className="h-svh flex flex-col items-center py-16 px-4 md:px-16 lg:px-32 xl:px-52">
+      <div className="flex flex-row w-full">
+        <Link
+          href={'/dashboard'}
+          className="flex items-center gap-1 text-sm text-primary-foreground m-5"
+        >
+          <ChevronLeft size={16} strokeWidth={2.25} />
+          <span>Back to Dashboard</span>
+        </Link>
+      </div>
       <div className="max-w-5xl">
         {profile ? (
           <ProfileCard profile={profile} showEditButton />
