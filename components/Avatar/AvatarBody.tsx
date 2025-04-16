d0be68bd650b11f6c1b563d255ef7b793827a053
Hector <baseballhector@gmail.com>
2025-04-16 16:01:31 -0400

    Added data-testid attribtute

--- components/Avatar/AvatarBody.tsx
+++ components/Avatar/AvatarBody.tsx
@@ -14,6 +14,7 @@ const AvatarBody = React.forwardRef<
       'relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full',
       className,
     )}
+    data-testid="avatar-body"
     {...props}
   />
 ));
