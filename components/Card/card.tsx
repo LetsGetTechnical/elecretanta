bb990421e2c56021ef2e995e64995511b9f05edf
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-04-16 10:59:13 -0500

    Fix: Handled ESLint Errors

--- components/Card/card.tsx
+++ components/Card/card.tsx
@@ -5,6 +5,12 @@ import { HTMLAttributes, forwardRef } from 'react';
 
 import { cn } from '@/lib/utils';
 
+/**
+ * Card component - container for card.
+ * @param {object} props - Props for the Card copmonent.
+ * @param {string} [props.className] - Additional class names.
+ * @returns {JSX.Element} - Rendered card container.
+ */
 const Card = forwardRef<
   HTMLDivElement,
   HTMLAttributes<HTMLDivElement>
@@ -20,6 +26,13 @@ const Card = forwardRef<
 ));
 Card.displayName = 'Card';
 
+/**
+ * Header portion of Card component.
+ * @param {object} props - Props for CardHeader component.
+ * @param {string} [props.className] - Additional class names for styling.
+ * @param {HTMLAttributes<HTMLDivElement>} ref - ref forwarded for header container.
+ * @returns {JSX.Element} - Rendered card header element. 
+ */
 const CardHeader = forwardRef<
   HTMLDivElement,
   HTMLAttributes<HTMLDivElement>
@@ -32,6 +45,13 @@ const CardHeader = forwardRef<
 ));
 CardHeader.displayName = 'CardHeader';
 
+/**
+ * Card title component used within Card.
+ * @param {object} props - Props for CardTitle component. 
+ * @param {string} [props.className] - Additional class names for styling. 
+ * @param {HTMLAttributes<HTMLDivElement>} ref - Ref forwarded to title div.
+ * @returns {JSX.Element} - Rendered card title element. 
+ */
 const CardTitle = forwardRef<
   HTMLDivElement,
   HTMLAttributes<HTMLDivElement>
@@ -44,6 +64,13 @@ const CardTitle = forwardRef<
 ));
 CardTitle.displayName = 'CardTitle';
 
+/**
+ * Card description component used within Card.
+ * @param {object} props - Props for CardDescription component. 
+ * @param {string} [props.className] - Additional class names for styling.
+ * @param {HTMLAttributes<HTMLDivElement>} ref - Ref forwarded to description div.
+ * @returns {JSX.Element} - Rendered card description element. 
+ */
 const CardDescription = forwardRef<
   HTMLDivElement,
   HTMLAttributes<HTMLDivElement>
@@ -56,6 +83,13 @@ const CardDescription = forwardRef<
 ));
 CardDescription.displayName = 'CardDescription';
 
+/**
+ * Container wrapping content within Card. 
+ * @param {object} props - Div props passed to content container.
+ * @param {string} [props.className] - Additional class names for styling. 
+ * @param {HTMLAttributes<HTMLDivElement>} ref - Ref forwarded to content div. 
+ * @returns {JSX.Element} - Rendered card content element. 
+ */
 const CardContent = forwardRef<
   HTMLDivElement,
   HTMLAttributes<HTMLDivElement>
@@ -64,6 +98,13 @@ const CardContent = forwardRef<
 ));
 CardContent.displayName = 'CardContent';
 
+/**
+ * Card footer component wrapping content within Card.
+ * @param {object} props - Div props passed to the footer container.
+ * @param {string} [props.className] - Additional class names for styling.
+ * @param {HTMLAttributes<HTMLDivElement>} ref - Ref forwarded to div element.
+ * @returns {JSX.Element} - The rendered card footer element. 
+ */
 const CardFooter = forwardRef<
   HTMLDivElement,
   HTMLAttributes<HTMLDivElement>
