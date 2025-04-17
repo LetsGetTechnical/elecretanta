3b10748c3436d6130d45adf322d45f82047cd316
Hector <baseballhector@gmail.com>
2025-04-16 20:55:27 -0400

    Added expect to test for img element

--- components/Avatar/AvatarFallback.test.tsx
+++ components/Avatar/AvatarFallback.test.tsx
@@ -5,12 +5,14 @@ import Avatar from './Avatar';
 import { render, screen } from '@testing-library/react';
 
 describe('AvatarFallback Component', () => {
-  it('should render correctly inside Avatar component', () => {
+  it('should render correctly with an image', () => {
     // Avatarfallback must be used within Avatar
     render(<Avatar userAvatar={''} />);
 
     const avatarFallbackElement = screen.getByTestId('avatar-fallback');
-
+    const imgElement = screen.getByAltText("default avatar");
+    
     expect(avatarFallbackElement).toBeInTheDocument();
+    expect(imgElement).toBeInTheDocument();
   });
 });
