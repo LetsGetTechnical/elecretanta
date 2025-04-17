8e6e3b0e99be76c88b07460daaa6b63b8a92161b
Hector <baseballhector@gmail.com>
2025-04-16 21:22:30 -0400

    Added expect to test that image has correct src attribute

--- components/Avatar/AvatarFallback.test.tsx
+++ components/Avatar/AvatarFallback.test.tsx
@@ -14,5 +14,6 @@ describe('AvatarFallback Component', () => {
     
     expect(avatarFallbackElement).toBeInTheDocument();
     expect(imgElement).toBeInTheDocument();
+    expect(imgElement).toHaveAttribute('src', 'https://static.vecteezy.com/system/resources/previews/024/183/525/non_2x/avatar-of-a-man-portrait-of-a-young-guy-illustration-of-male-character-in-modern-color-style-vector.jpg');
   });
 });
