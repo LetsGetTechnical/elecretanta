b38a151d94b5e483ada855f1a8e00c7f11931668
Alex Appleget <alexappleget2014@gmail.com>
2025-04-18 17:58:34 -0500

    Fix: testing vercel unit test build

--- app/api/cron/functions/checkAuthorization/checkCronAuthorization.ts
+++ app/api/cron/functions/checkAuthorization/checkCronAuthorization.ts
@@ -8,5 +8,6 @@
  */
 export const checkCronAuthorization = (request: Request): boolean => {
   const authHeader = request.headers.get('authorization');
+
   return authHeader === `Bearer ${process.env.CRON_SECRET}`;
 };
