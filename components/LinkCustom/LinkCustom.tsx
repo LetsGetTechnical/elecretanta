18a535f0a735277b182d3d64828b236ce358ed80
Cody Epstein <codyepstein@gmail.com>
2025-04-21 20:50:20 -0700

    change type to interface and move into new file

--- components/LinkCustom/LinkCustom.tsx
+++ components/LinkCustom/LinkCustom.tsx
@@ -3,25 +3,20 @@
 
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
-import { ReactNode, JSX } from 'react';
-type NavLinkProps = {
-  href: string;
-  children?: ReactNode;
-  className?: string;
-};
-
+import { JSX } from 'react';
+import { ILinkCustomProps } from './ILinkCustomProps';
 /**
  * A navigation link component that uses Next.js Link component.
  * @param {string} href - The URL to link to.
  * @param {ReactNode} [children] - The content to display inside the link.
  * @param {string} [className] - The class name to apply to the link.
- * @returns A navigation link component.
+ * @returns A custom navigation link component.
  */
 const LinkCustom = ({
   href,
   children,
   className,
-}: NavLinkProps): JSX.Element => {
+}: ILinkCustomProps): JSX.Element => {
   return (
     <Link
       href={href}
