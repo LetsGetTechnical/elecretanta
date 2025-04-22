52e32dec196f054916d2ef6294d9cc5f95e2cd7a
Zoobia Syyed <syyedzoobia@gmail.com>
2025-04-22 11:26:18 -0700

    more changes made, pnpm i

--- components/TextArea/textarea.tsx
+++ components/TextArea/textarea.tsx
@@ -9,7 +9,7 @@ import { cn } from '@/lib/utils';
  * @param {ComponentProps<'textarea'>} props
  * @param {string} [props.className]
  * @returns {JSX.Element}
- */
+ **/
 
 const Textarea = forwardRef<
   HTMLTextAreaElement,
@@ -21,7 +21,7 @@ const Textarea = forwardRef<
         'flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
         className,
       )}
-      ref={ref} 
+      ref={ref}
       {...props}
     />
   );
