912051dcbf0c6311f2c8cf1c97d4ea4e404203f4
Will Cooley <william.cooley@gmail.com>
2025-04-23 22:41:14 -0400

    docs: add jsdoc comments. add header. add missing return type. change to arrow function. describe context variable and disabled prop

--- components/SnowOverlayWrapper/SnowOverlayWrapper.tsx
+++ components/SnowOverlayWrapper/SnowOverlayWrapper.tsx
@@ -1,9 +1,22 @@
+// Copyright (c) Gridiron Survivor.
+// Licensed under the MIT License.
+
 'use client';
 
+import { JSX } from 'react';
 import { useSnowOverlay } from '@/providers/SnowOverlayProvider';
 import { SnowOverlay } from 'react-snow-overlay';
 
-export default function SnowOverlayWrapper() {
+/**
+ * Wrapper than renders the visual snow effect of SnowOverlay.
+ * `isSnowing`: A boolean state derived from useSnowOverlay context to toggle snowing.
+ * `disabledOnSingleCpuDevices`: A boolean prop that disables the snow effect on devices with a single CPU core.
+ * @returns {JSX.Element} - The rendered SnowOverlayWrapper element.
+ */
+const SnowOverlayWrapper = (): JSX.Element => {
   const { isSnowing } = useSnowOverlay();
   return <SnowOverlay disabledOnSingleCpuDevices disabled={!isSnowing} />;
 }
+
+export default SnowOverlayWrapper
+
