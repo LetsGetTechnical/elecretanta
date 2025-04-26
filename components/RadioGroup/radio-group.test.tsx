38ad0fd0383aa7fa5e07ea9ebedf0acef8aa30ea
Hector <baseballhector@gmail.com>
2025-04-26 17:59:14 -0400

    Initial unit tests for RadioGroup

---
+++ components/RadioGroup/radio-group.test.tsx
@@ -0,0 +1,42 @@
+// Copyright (c) Gridiron Survivor.
+// Licensed under the MIT License.
+
+import { Root as RadioGroupPrimitiveRoot } from '@radix-ui/react-radio-group';
+import { RadioGroup, RadioGroupItem } from './radio-group';
+import { render, screen } from '@testing-library/react';
+
+describe('Radio Group Component', () => {
+  it('renders radio group correctly with passed children and default classes', () => {
+    render(
+      <RadioGroup>
+        <div>Test child</div>
+      </RadioGroup>,
+    );
+
+    const radioGroupElement = screen.getByTestId('radio-group');
+    const testChildElement = screen.getByText('Test child');
+
+    expect(radioGroupElement).toBeInTheDocument();
+    expect(radioGroupElement).toHaveClass('grid gap-2');
+    expect(testChildElement).toBeInTheDocument();
+  });
+
+  it('renders radio group with additional custom classes', () => {
+    render(<RadioGroup className="custom-class-1 custom-class-2" />);
+
+    const radioGroupElement = screen.getByTestId('radio-group');
+
+    expect(radioGroupElement).toBeInTheDocument();
+    expect(radioGroupElement).toHaveClass('grid gap-2');
+    expect(radioGroupElement).toHaveClass('custom-class-1 custom-class-2');
+  });
+
+  it('renders an empty radio group', () => {
+    render(<RadioGroup />);
+
+    const radioGroupElement = screen.getByTestId('radio-group');
+
+    expect(radioGroupElement).toBeEmptyDOMElement();
+  });
+});
+
