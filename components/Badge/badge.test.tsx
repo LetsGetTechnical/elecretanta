fa09669d42747bae9a7cdf944b79e15ee3541662
Khurram Ali <khurramcali@gmail.com>
2025-04-26 16:24:16 -0400

    refactor: button variants to be used for test

--- components/Badge/badge.test.tsx
+++ components/Badge/badge.test.tsx
@@ -3,34 +3,26 @@
 
 import { Badge, BadgeProps, badgeVariants } from './badge';
 import { render, screen } from '@testing-library/react';
+import { variants } from './badge';
 
-const variantTestCases = [
-  'default',
-  'destructive',
-  'outline',
-  'secondary',
-] as NonNullable<BadgeProps['variant']>[];
+const variantNames = Object.keys(variants.variant) as NonNullable<
+  BadgeProps['variant']
+>[];
 
 describe('Badge', () => {
-  it('renders the component in the document', () => {
+  it('renders the component with default variant', () => {
     render(<Badge />);
 
     const badge = screen.getByTestId('badge');
 
     expect(badge).toBeInTheDocument();
-  });
-
-  it('renders with default variant when none is provided', () => {
-    render(<Badge />);
-
-    const badge = screen.getByTestId('badge');
 
     const classNames = badgeVariants({ variant: 'default' });
     expect(badge).toHaveClass(classNames);
   });
 
-  for (let i = 0; i < variantTestCases.length; i++) {
-    const variant = variantTestCases[i];
+  for (let i = 0; i < variantNames.length; i++) {
+    const variant = variantNames[i];
     it(`renders with the "${variant}" variant and applies correct styles`, () => {
       render(<Badge variant={variant} />);
 
--- components/Badge/badge.tsx
+++ components/Badge/badge.tsx
@@ -6,19 +6,21 @@ import { HTMLAttributes, JSX } from 'react';
 
 import { cn } from '@/lib/utils';
 
+export const variants = {
+  variant: {
+    default: 'border-transparent bg-primary text-primary-foreground shadow',
+    secondary:
+      'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
+    destructive:
+      'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
+    outline: 'text-foreground',
+  },
+};
+
 const badgeVariants = cva(
   'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
-    variants: {
-      variant: {
-        default: 'border-transparent bg-primary text-primary-foreground shadow',
-        secondary:
-          'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        destructive:
-          'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
-        outline: 'text-foreground',
-      },
-    },
+    variants,
     defaultVariants: {
       variant: 'default',
     },
