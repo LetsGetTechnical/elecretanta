98f5c81eb7d411044590083d1a642e370becc20e
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-04-27 13:58:30 -0500

    fix: added missing JSDoc comment and return type on function

--- components/NavLogo/NavLogo.tsx
+++ components/NavLogo/NavLogo.tsx
@@ -3,8 +3,13 @@
 
 import { Gift } from 'lucide-react';
 import Link from 'next/link';
+import { JSX } from 'react';
 
-const NavLogo = () => {
+/**
+ * Renders the logo in the navigation bar, which redirects to the dashboard when clicked.
+ * @returns { JSX.Element } The rendered navigation logo. 
+ */
+const NavLogo = (): JSX.Element => {
   return (
     <Link href={'/dashboard'} className="flex items-center">
       <Gift className="bg-logoWhite h-8 w-8 p-1 rounded-lg text-red-600" />
