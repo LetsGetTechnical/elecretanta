dbe71795936da9acfdea1659c4547054c22581a3
Zoobia Syyed <syyedzoobia@gmail.com>
2025-04-28 14:56:45 -0700

    removed test file

--- components/SnowOverlayToggle/SnowOverlayToggle.test.tsx
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Gridiron Survivor.
-// Licensed under the MIT License.
-
-import { render, screen } from '@testing-library/react';
-import SnowOverlayToggle from './SnowOverlayToggle';
