b04cff3b8bb6ab465e5c80b595d6c49f48910341
Will Cooley <william.cooley@gmail.com>
2025-04-29 13:27:24 -0400

    test: add 2 tests that check if the correct props get passed to SnowOverlay

--- components/SnowOverlayWrapper/SnowOverlayWrapper.test.tsx
+++ components/SnowOverlayWrapper/SnowOverlayWrapper.test.tsx
@@ -1,12 +1,58 @@
 // Copyright (c) Gridiron Survivor.
 // Licensed under the MIT License.
 
+import { JSX } from 'react';
 import SnowOverlayWrapper from "./SnowOverlayWrapper";
 import { render, screen } from '@testing-library/react'
+
+const mockSnowState = { isSnowing: false}
+
+jest.mock('@/providers/SnowOverlayProvider', ()=>({
+    useSnowOverlay: () => mockSnowState
+}))
+
+const mockSnowOverlay = jest.fn()
+
+interface SnowOverlayProps {
+    disabled: boolean;
+    disabledOnSingleCpuDevices: boolean;
+}
+
+jest.mock("react-snow-overlay", ()=>({
+    SnowOverlay: (props: SnowOverlayProps): JSX.Element => {
+        mockSnowOverlay(props);
+        return <div />
+    }
+}));
+
 describe('SnowOverlayWrapper', ()=>{
+    beforeEach(()=>{
+        mockSnowOverlay.mockClear()
+        mockSnowState.isSnowing = false
+    })
     it('Renders the snowOverlayWrapper', ()=>{
         render(<SnowOverlayWrapper />)
         const testSnowWrapper = screen.getByTestId("snowOverlayWrapper")
         expect(testSnowWrapper).toBeInTheDocument()
+    })
+    it('passes down disabled=true prop to SnowOverlay when isSnowing=false', ()=>{
+        mockSnowState.isSnowing = false
+        render(<SnowOverlayWrapper />)
+        expect(mockSnowOverlay).toHaveBeenCalledWith(
+            expect.objectContaining({ 
+                disabled: true, 
+                "disabledOnSingleCpuDevices": true, 
+            })
+        );
+    })
+    it('passes down disabled=false prop to SnowOverlay when isSnowing=true', ()=>{
+        mockSnowState.isSnowing = true
+        render(<SnowOverlayWrapper />)
+        expect(mockSnowOverlay).toHaveBeenCalledWith(
+            expect.objectContaining({ 
+                disabled: false, 
+                "disabledOnSingleCpuDevices": true, 
+            })
+        );
     })
 })
