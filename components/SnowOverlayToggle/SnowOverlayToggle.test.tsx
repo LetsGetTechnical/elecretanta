2057375259e4256aa559ba8e1e707e5e5473aed3
Zoobia Syyed <syyedzoobia@gmail.com>
2025-04-30 15:34:49 -0700

    completed unit test with all checks passing

--- components/SnowOverlayToggle/SnowOverlayToggle.test.tsx
+++ components/SnowOverlayToggle/SnowOverlayToggle.test.tsx
@@ -1,15 +1,25 @@
 // Copyright (c) Gridiron Survivor.
 // Licensed under the MIT License.
 
+
 import { SnowOverlayToggle } from "./SnowOverlayToggle";
 import { fireEvent, render, screen } from '@testing-library/react';
+import { SnowOverlayProvider } from "@/providers/SnowOverlayProvider";
 
 
 describe('SnowOverlayToggle', () => {
   it('Toggles the snowfall overlay', ()=> {
-    render(<SnowOverlayToggle/>)
-    expect(screen.queryByTestId('snow-icon')).toBeInTheDocument();
+    render(
+      <SnowOverlayProvider>
+        <SnowOverlayToggle />
+      </SnowOverlayProvider>,
+    );
+    const sunIcon = screen.getByTestId('sun-icon');
+    expect(sunIcon).toBeInTheDocument();
+
     fireEvent.click(screen.getByTestId('snow-overlay-toggle'));
-    expect(screen.queryByTestId('sun-icon')).toBeInTheDocument();
+
+    const snowIcon = screen.getByTestId('snow-icon');
+    expect(snowIcon).toBeInTheDocument();
   })
 })
