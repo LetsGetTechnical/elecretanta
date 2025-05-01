e5517f614212a600c5016063461177fd32df7126
Will Cooley <william.cooley@gmail.com>
2025-04-30 22:03:18 -0400

    test: add test - renders the textarea with props

--- components/TextArea/textarea.test.tsx
+++ components/TextArea/textarea.test.tsx
@@ -5,14 +5,26 @@ import { render, screen } from '@testing-library/react';
 import { Textarea } from './textarea';
 
 describe("Textarea", () => {
-    it("renders the Textarea correctly", () => {
+    it("Renders the Textarea correctly", () => {
         render(<Textarea />)
         const textarea = screen.getByRole("textbox")
         expect(textarea).toBeInTheDocument()
     })
-    it("renders the Textarea with custom className", ()=>{
+    it("Renders the Textarea with custom className", ()=>{
         render(<Textarea className="custom-cn" />)
         const textarea = screen.getByRole("textbox")
         expect(textarea).toHaveClass("custom-cn")
+    })
+    it("Renders the Textarea with props", ()=>{
+        render(
+            <Textarea 
+                placeholder="test-placeholder"
+                name="test-name"
+                defaultValue="test-value"
+            />)
+        const textarea = screen.getByRole("textbox")
+        expect(textarea).toHaveAttribute("placeholder", "test-placeholder")
+        expect(textarea).toHaveAttribute("name", "test-name")
+        expect(textarea).toHaveValue("test-value")
     })
 })
