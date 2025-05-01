8797d9caf4d09d667388e8016500b43547809b30
Hector <baseballhector@gmail.com>
2025-05-01 19:40:00 -0400

    Added test to check for custom attributes

--- components/Avatar/AvatarBody/AvatarBody.test.tsx
+++ components/Avatar/AvatarBody/AvatarBody.test.tsx
@@ -38,4 +38,12 @@ describe('AvatarBody Component', () => {
 
     expect(avatarBodyElement).toBeEmptyDOMElement();
   });
+
+  it('renders with custom attributes', () => {
+    render(<AvatarBody data-custom-prop="customProp" />);
+
+    const avatarBodyElement = screen.getByTestId('avatar-body');
+
+    expect(avatarBodyElement).toHaveAttribute('data-custom-prop', 'customProp');
+  });
 });
