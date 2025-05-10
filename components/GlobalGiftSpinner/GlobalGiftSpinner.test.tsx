d1b4abbc7bec3153358061a7f829d4eedea19bb1
Julianna Messineo <juliannamessineo@gmail.com>
2025-05-10 13:21:49 -0500

    Test: Add tests for midway through animation

--- components/GlobalGiftSpinner/GlobalGiftSpinner.test.tsx
+++ components/GlobalGiftSpinner/GlobalGiftSpinner.test.tsx
@@ -1,19 +1,15 @@
-// Initially pathLength is 0
-// Initially opacity is 0
-// When the component mounts, the pathLength should be 1
-// When the component mounts, the opacity should be 1
-
 import { render, screen } from '@testing-library/react';
 import GlobalGiftSpinner from './GlobalGiftSpinner';
 
 describe('GlobalGiftSpinner', () => {
+
   it('should render', () => {
     render(<GlobalGiftSpinner />);
     const spinner = screen.getByTestId('global-gift-spinner');
     expect(spinner).toBeInTheDocument();
   });
 
-  it('Initially the pathLength is 0 and opacity is 0', () => {
+  it('Initially the stroke-dasharray is 0px 1px and opacity is 0', () => {
     render(<GlobalGiftSpinner />);
     const giftIconRightSide = screen.getByTestId('gift-icon-right-side');
     const giftIconLeftSide = screen.getByTestId('gift-icon-left-side');
@@ -23,17 +19,43 @@ describe('GlobalGiftSpinner', () => {
     expect(giftIconLeftSide).toHaveAttribute('opacity', '0');
   });
 
-  it('After the animation is complete, the pathLength is 1 and opacity is 1', () => {
+  it('After the animation is complete, the stroke-dasharray is 1px 1px and opacity is 1', () => {
     render(<GlobalGiftSpinner />);
-    setTimeout(() => {
       const giftIconRightSide = screen.getByTestId('gift-icon-right-side');
       const giftIconLeftSide = screen.getByTestId('gift-icon-left-side');
-      screen.debug(giftIconRightSide);
-      expect(giftIconRightSide).toHaveAttribute('stroke-dasharray', '1px 1px');
-      expect(giftIconLeftSide).toHaveAttribute('stroke-dasharray', '1px 1px');
-      expect(giftIconRightSide).toHaveAttribute('opacity', '1');
-      expect(giftIconLeftSide).toHaveAttribute('opacity', '1');
-    }, 2000);
+      setTimeout(() => {
+        expect(giftIconRightSide).toHaveAttribute('stroke-dasharray', '1px 1px');
+        expect(giftIconLeftSide).toHaveAttribute('stroke-dasharray', '1px 1px');
+        expect(giftIconRightSide).toHaveAttribute('opacity', '1');
+        expect(giftIconLeftSide).toHaveAttribute('opacity', '1');
+      }, 1500);
+  });
+
+  it('Partway through the animation, the first value of the stroke-dasharray is greater than 0 and less than 1', () => {
+    render(<GlobalGiftSpinner />);
+      const giftIconRightSide = screen.getByTestId('gift-icon-right-side');
+      const giftIconLeftSide = screen.getByTestId('gift-icon-left-side');
+      setTimeout(() => {            
+        const strokeDashArray = giftIconRightSide.getAttribute('stroke-dasharray');
+        const strokeDashArrayArray = strokeDashArray?.split(' ');
+        const strokeDashArrayValue = parseFloat(strokeDashArrayArray?.[0] || '0');
+        expect(strokeDashArrayValue).toBeGreaterThan(0);
+        expect(strokeDashArrayValue).toBeLessThan(1);
+      }, 200);
+  });
+
+  it('The animation restarts after the first animation is complete', () => {
+    render(<GlobalGiftSpinner />);
+      const giftIconRightSide = screen.getByTestId('gift-icon-right-side');
+      const giftIconLeftSide = screen.getByTestId('gift-icon-left-side');
+      setTimeout(() => {            
+        const strokeDashArray = giftIconRightSide.getAttribute('stroke-dasharray');
+        const strokeDashArrayArray = strokeDashArray?.split(' ');
+        const strokeDashArrayValue = parseFloat(strokeDashArrayArray?.[0] || '0');
+        expect(strokeDashArrayValue).toBeGreaterThan(0);
+        expect(strokeDashArrayValue).toBeLessThan(1);
+      }, 1700);
   });
 });
+
 
