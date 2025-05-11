4d09e8c18c95d1b40076e4de8f0f58ad66fffd1c
Hector <baseballhector@gmail.com>
2025-05-11 14:46:48 -0400

    fix: Removed JSDocs

--- app/types/giftSuggestion.ts
+++ app/types/giftSuggestion.ts
@@ -1,17 +1,6 @@
 // Copyright (c) Gridiron Survivor.
 // Licensed under the MIT License.
 
-/**
- * Interface for a gift suggestion.
- * @typedef {object} GiftSuggestion
- * @property {string} id - The unique identifier for the gift suggestion.
- * @property {string} title - The title of the gift suggestion.
- * @property {string} price - The price of the gift suggestion.
- * @property {string} description - The description of the gift suggestion.
- * @property {string[]} matchReasons - An array of reasons why the gift suggestion matches the profile.
- * @property {number} matchScore - The match score of the gift suggestion.
- * @property {string | null} imageUrl - The image URL of the gift suggestion.
- */
 export interface GiftSuggestion {
   id: string;
   title: string;
