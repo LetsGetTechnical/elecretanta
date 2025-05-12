d0629b532723c55a1c2dd2a3ceb02a7d6403e0fd
haydnkerr <haydn.kerr@uconn.edu>
2025-05-12 10:35:49 -0400

    Minimized code needed to test size and variants
    used test.each() to loop through types of buttons instead of listing out every button test

--- components/Button/button.test.tsx
+++ components/Button/button.test.tsx
@@ -15,65 +15,29 @@ describe("Button", () => {
         expect(button).toHaveClass('custom-class');
       });
 
-      it('renders with variant for default button', () => {
-        render(<Button variant="default" data-testid="button">Test</Button>);
+      test.each([
+        ["default", "bg-primary text-primary-foreground shadow"],
+        ["secondary", "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80"],
+        ["destructive", "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90"],
+        ["outline", "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground"],
+        ["ghost", "hover:bg-accent hover:text-accent-foreground"],
+        ["link", "text-primary underline-offset-4 hover:underline"],
+      ])("Renders with variant='%s'", (btnVariant, expectedClass) => {
+        render(<Button variant={btnVariant} data-testid="button">Test</Button>);
         const button = screen.getByTestId('button');
-        expect(button).toHaveClass('bg-primary text-primary-foreground shadow');
-      });
+        expect(button).toHaveClass(expectedClass);
+      })
 
-      it('renders with variant for delete button', () => {
-        render(<Button variant="destructive" data-testid="button">Test</Button>);
+      test.each([
+        ["default","h-9 px-4 py-2"],
+        ["sm","h-8 rounded-md px-3 text-xs"],
+        ["lg","h-10 rounded-md px-8"],
+        ["icon","h-9 w-9"]
+      ])
+        ('renders with size="%s"', (btnSize, expectedClass) => {
+        render(<Button size={btnSize} data-testid="button">Test</Button>);
         const button = screen.getByTestId('button');
-        expect(button).toHaveClass('bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90');
-      });
-
-      it('renders with variant for outline button', () => {
-        render(<Button variant="outline" data-testid="button">Test</Button>);
-        const button = screen.getByTestId('button');
-        expect(button).toHaveClass('border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground');
-      });
-
-      it('renders with variant for ghost button', () => {
-        render(<Button variant="ghost" data-testid="button">Test</Button>);
-        const button = screen.getByTestId('button');
-        expect(button).toHaveClass('hover:bg-accent hover:text-accent-foreground');
-
-      });
-
-      it('renders with variant for secondary button', () => {
-        render(<Button variant="secondary" data-testid="button">Test</Button>);
-        const button = screen.getByTestId('button');
-        expect(button).toHaveClass('bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80');
-      });
-
-      it('renders with variant for link', () => {
-        render(<Button variant="link" data-testid="button">Test</Button>);
-        const button = screen.getByTestId('button');
-        expect(button).toHaveClass('text-primary underline-offset-4 hover:underline');
-      });
-
-      it('renders with size default', () => {
-        render(<Button size="default" data-testid="button">Test</Button>);
-        const button = screen.getByTestId('button');
-        expect(button).toHaveClass('h-9 px-4 py-2');
-      });
-
-      it('renders with size for small button', () => {
-        render(<Button size="sm" data-testid="button">Test</Button>);
-        const button = screen.getByTestId('button');
-        expect(button).toHaveClass('h-8 rounded-md px-3 text-xs');
-      });
-
-      it('renders with size for large button', () => {
-        render(<Button size="lg" data-testid="button">Test</Button>);
-        const button = screen.getByTestId('button');
-        expect(button).toHaveClass('h-10 rounded-md px-8');
-      });
-
-      it('renders with size for icon button', () => {
-        render(<Button size="icon" data-testid="button">Test</Button>);
-        const button = screen.getByTestId('button');
-        expect(button).toHaveClass('h-9 w-9');
+        expect(button).toHaveClass(expectedClass);
       });
 
     it("triggers onClick behavior with mouse click", async () => {
