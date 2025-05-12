aac36fc3975107d70b7a915d55fbea8a093892e6
Alex Appleget <alexappleget2014@gmail.com>
2025-05-12 12:51:57 -0500

    [#152] Test: created integration test for supabase/middleware

--- lib/supabase/middleware/middleware.ts
+++ lib/supabase/middleware/middleware.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Gridiron Survivor.
+// Licensed under the MIT License.
+
 import { createServerClient } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
@@ -29,15 +32,10 @@ export async function updateSession(request: NextRequest) {
     },
   );
 
-  // IMPORTANT: Avoid writing any logic between createServerClient and
-  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
-  // issues with users being randomly logged out.
-
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  // If the user is not authenticated redirect the user to the login page
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/auth/login') &&
@@ -46,23 +44,18 @@ export async function updateSession(request: NextRequest) {
     request.nextUrl.pathname !== '/' &&
     !request.nextUrl.pathname.startsWith('/gift-exchanges')
   ) {
-    // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = '/';
     return NextResponse.redirect(url);
-    // Todo: Uncomment this to redirect unauthenticated users to the login page
   }
 
-  // if the user is authenticated check if they have already onboarded
   if (user) {
-    // Get the user's profile
     const { data: profile } = await supabase
       .from('profiles')
       .select('onboarding_complete')
       .eq('id', user.id)
       .single();
 
-    // If user is not onboarded and not already on the onboarding page, redirect to onboarding
     if (
       profile &&
       !profile.onboarding_complete &&
@@ -73,19 +66,6 @@ export async function updateSession(request: NextRequest) {
       return NextResponse.redirect(url);
     }
   }
-
-  // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
-  // creating a new response object with NextResponse.next() make sure to:
-  // 1. Pass the request in it, like so:
-  //    const myNewResponse = NextResponse.next({ request })
-  // 2. Copy over the cookies, like so:
-  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-  // 3. Change the myNewResponse object to fit your needs, but avoid changing
-  //    the cookies!
-  // 4. Finally:
-  //    return myNewResponse
-  // If this is not done, you may be causing the browser and server to go out
-  // of sync and terminate the user's session prematurely!
 
   return supabaseResponse;
 }
