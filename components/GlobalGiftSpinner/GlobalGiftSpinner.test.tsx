223a87256ffa47a14755a0d73927a27c61ab3fe5
Julianna Messineo <juliannamessineo@gmail.com>
2025-05-12 11:42:24 -0500

    Chore: Added comment to test file

--- components/GlobalGiftSpinner/GlobalGiftSpinner.test.tsx
+++ components/GlobalGiftSpinner/GlobalGiftSpinner.test.tsx
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import GlobalGiftSpinner from './GlobalGiftSpinner';
 
+/**
+ * Although this component shows changing the pathLength from 0 to 1, framer motion is actually changing the stroke-dasharray svg property from 0px 1px to 1px 1px and leaving the pathLength constant. 
+ */
 describe('GlobalGiftSpinner', () => {
 
   it('should render', () => {
