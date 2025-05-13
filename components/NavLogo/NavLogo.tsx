c4942d10c3a51f3786a72250540643728b24454d
luisarevalo21 <arevaloluis89@yahoo.com>
2025-05-12 18:00:26 -0700

    revered

--- components/NavLogo/NavLogo.tsx
+++ components/NavLogo/NavLogo.tsx
@@ -11,19 +11,9 @@ import { JSX } from 'react';
  */
 const NavLogo = (): JSX.Element => {
   return (
-    <Link 
-      href={'/dashboard'} 
-      className="flex items-center"
-      data-testid="nav-logo-link"
-    >
-      <Gift 
-        className="bg-logoWhite h-8 w-8 p-1 rounded-lg text-red-600" 
-        data-testid="nav-logo-icon"
-      />
-      <p 
-        className="font-bold ml-2 italic text-green-200"
-        data-testid="nav-logo-text"
-      >
+    <Link href={'/dashboard'} className="flex items-center">
+      <Gift className="bg-logoWhite h-8 w-8 p-1 rounded-lg text-red-600" />
+      <p className="font-bold ml-2 italic text-green-200">
         Elf<span className="text-white font-semibold">gorithm</span>
       </p>
     </Link>
