9f9961cb18849fe5653aa13485394db38b5333a1
Will Cooley <william.cooley@gmail.com>
2025-05-13 10:23:14 -0400

    test: use data-testid instead of getByRole to find elements being tested to meet style guide requirements

--- components/TextArea/textarea.test.tsx
+++ components/TextArea/textarea.test.tsx
@@ -8,12 +8,12 @@ import { Textarea } from './textarea';
 describe("Textarea", () => {
     it("Renders the Textarea correctly", () => {
         render(<Textarea />)
-        const textarea = screen.getByRole("textbox")
+        const textarea = screen.getByTestId("textarea")
         expect(textarea).toBeInTheDocument()
     })
     it("Renders the Textarea with custom className", ()=>{
         render(<Textarea className="custom-cn" />)
-        const textarea = screen.getByRole("textbox")
+        const textarea = screen.getByTestId("textarea")
         expect(textarea).toHaveClass("custom-cn")
     })
     it("Renders the Textarea with props", ()=>{
@@ -23,14 +23,14 @@ describe("Textarea", () => {
                 name="test-name"
                 defaultValue="test-value"
             />)
-        const textarea = screen.getByRole("textbox")
+        const textarea = screen.getByTestId("textarea")
         expect(textarea).toHaveAttribute("placeholder", "test-placeholder")
         expect(textarea).toHaveAttribute("name", "test-name")
         expect(textarea).toHaveValue("test-value")
     })
     it("Should be able to type into Textarea", async () => {
         render(<Textarea />)
-        const textareaElement = screen.getByRole<HTMLTextAreaElement>("textbox")
+        const textareaElement = screen.getByTestId<HTMLTextAreaElement>("textarea")
         await userEvent.type(textareaElement, 'testing the textarea element')
         expect(textareaElement.value).toBe("testing the textarea element")
     })
