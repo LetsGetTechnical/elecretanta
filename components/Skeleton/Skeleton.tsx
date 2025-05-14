fac4dfcd728e099cc27df36cdaf8ebe989a8c2dc
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-05-13 23:59:46 -0500

    chore: rename SkeletonProps to ISkeletonProps to indicate interface

--- components/Skeleton/Skeleton.tsx
+++ components/Skeleton/Skeleton.tsx
@@ -3,17 +3,17 @@
 
 import { JSX } from "react";
 
-interface SkeletonProps {
+interface ISkeletonProps {
   className?: string;
 }
 
 /**
  * Skeleton component - animated placeholder for loading states. 
- * @param {SkeletonProps} props - The component props.
+ * @param {ISkeletonProps} props - The component props.
  * @param {string} [props.className] - Additional classNames for styling.
  * @returns {JSX.Element} Styled div serving as a loading placeholder.
  */
-export const Skeleton = ({ className }: SkeletonProps): JSX.Element => {
+export const Skeleton = ({ className }: ISkeletonProps): JSX.Element => {
   return (
     <div className={`animate-pulse bg-gray-200 rounded-md ${className}`} />
   );
