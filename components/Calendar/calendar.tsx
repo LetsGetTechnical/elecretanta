e0edecbf888f05a34fdc2819ca204b88769465b8
haydnkerr <haydn.kerr@uconn.edu>
2025-05-18 15:11:22 -0400

    Passed temporary props through to chevrons
    To address ESlint errors, passed props through the directional chevrons that can later be utilized

--- components/Calendar/calendar.tsx
+++ components/Calendar/calendar.tsx
@@ -3,7 +3,7 @@
 
 'use client';
 
-import * as React from 'react';
+import React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { DayPicker } from 'react-day-picker';
 import { JSX } from 'react';
@@ -72,19 +72,18 @@ const Calendar = ({
         ...classNames,
       }}
       components={{
-        // The lack of props is causing a linter error, but we will need them when setting up the fetch logic later. For now I'm commenting out the error as a temporary fix.
-        // IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        // IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
         /**
          * Custom component for rendering the left icon
+         * @param {object} props - Props passed to component
          * @returns {JSX.Element} The rendered ChevronLeft icon with class `h-4 w-4`.
          */
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconLeft: (props) => <ChevronLeft {...props} className="h-4 w-4" />,
         /**
          * Custom component for rendering the right icon
+         * @param {object} props - Props passed to component
          * @returns {JSX.Element} The rendered ChevronRight icon with class `h-4 w-4`.
          */
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
+        IconRight: (props) => <ChevronRight {...props}  className="h-4 w-4" />,
       }}
       {...props}
     />
