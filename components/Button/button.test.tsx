e027c4cbfd1efd4fa440db67c3db91d7e596fb31
haydnkerr <haydn.kerr@uconn.edu>
2025-05-19 12:27:12 -0400

    Update Button-test descriptions

--- components/Button/button.test.tsx
+++ components/Button/button.test.tsx
@@ -3,13 +3,13 @@ import userEvent from '@testing-library/user-event';
 import { Button } from './button';
 
 describe("Button", () => {
-    it("renders a button element with default props", () => {
+    it("renders the Button Component", () => {
         render(<Button data-testid="button"/>);
         const testButton = screen.getByTestId('button');
         expect(testButton).toBeInTheDocument();
     });
 
-    it('renders a button element with custom className', () => {
+    it('renders the Button Component with custom className', () => {
         render(<Button className="custom-class" data-testid="button">Test</Button>);
         const button = screen.getByTestId('button');
         expect(button).toHaveClass('custom-class');
@@ -22,7 +22,7 @@ describe("Button", () => {
         ["outline", "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground"],
         ["ghost", "hover:bg-accent hover:text-accent-foreground"],
         ["link", "text-primary underline-offset-4 hover:underline"],
-      ])("Renders a button element with variant='%s'", (btnVariant, expectedClass) => {
+      ])("Renders the Button Component with variant='%s'", (btnVariant, expectedClass) => {
         render(<Button variant={btnVariant} data-testid="button">Test</Button>);
         const button = screen.getByTestId('button');
         expect(button).toHaveClass(expectedClass);
@@ -34,13 +34,13 @@ describe("Button", () => {
         ["lg","h-10 rounded-md px-8"],
         ["icon","h-9 w-9"]
       ])
-        ('renders a button element with size="%s"', (btnSize, expectedClass) => {
+        ('renders the Button Component with size="%s"', (btnSize, expectedClass) => {
         render(<Button size={btnSize} data-testid="button">Test</Button>);
         const button = screen.getByTestId('button');
         expect(button).toHaveClass(expectedClass);
       });
 
-    it("triggers button onClick behavior with mouse click", async () => {
+    it("triggers Button component onClick behavior with mouse click", async () => {
         const handleClick = jest.fn(); 
         render(<Button data-testid="button" onClick={handleClick} />);
         const testButton = screen.getByTestId('button');
@@ -48,7 +48,7 @@ describe("Button", () => {
         expect(handleClick).toHaveBeenCalledTimes(1); 
     });
 
-    it("triggers button onClick behavior with keyboard enter press",async () => {
+    it("triggers Button component onClick behavior with keyboard enter press",async () => {
       const handleClick = jest.fn(); 
       render(<Button data-testid="button" onClick={handleClick} />);
       const testButton = screen.getByTestId('button');
