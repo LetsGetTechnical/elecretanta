20e00a7ecc9e5cc410e198ca2b725140c15d43c3
Will Cooley <william.cooley@gmail.com>
2025-05-20 11:26:40 -0400

    test: add navigation test for editProfileButton

--- components/ProfileCard/ProfileCard.test.tsx
+++ components/ProfileCard/ProfileCard.test.tsx
@@ -4,28 +4,33 @@
 import { render, screen } from '@testing-library/react';
 import ProfileCard from './ProfileCard';
 import { Profile } from '@/app/types/profile';
+import { userEvent } from '@testing-library/user-event';
+
+const mockPush = jest.fn();
 
 const mockProfile: Profile = {
-  id: "2",
-  display_name:"'Will Cooley",
-  email: "will@aol.com",
-  age_group: "65+",
-  avatar: "will.jpg",
-  categories: ["Music & Sound", "Tech & Gadgets"],
-  hobbies: "unit testing",
-  avoid: "spiders",
+  id: '2',
+  display_name: "will cooley",
+  email: 'will@aol.com',
+  age_group: '65+',
+  avatar: 'will.jpg',
+  categories: ['Music & Sound', 'Tech & Gadgets'],
+  hobbies: 'unit testing',
+  avoid: 'spiders',
   practical_whimsical: 3,
   cozy_adventurous: 3,
   minimal_luxurious: 4,
-  created_at: "jan 1 2025",
-  updated_at: "jan 2 2025",
+  created_at: 'jan 1 2025',
+  updated_at: 'jan 2 2025',
   onboarding_complete: true,
 };
 
 jest.mock('next/navigation', () => ({
   useRouter() {
-    return { prefetch: () => null };
-  },
+    return {
+      push: mockPush,
+    }
+  }
 }));
 
 describe('ProfileCard', () => {
@@ -35,4 +40,11 @@ describe('ProfileCard', () => {
     expect(profileName).toHaveTextContent('Will Cooley');
   });
 
+  it('Edit Profile Button navigates to the onboarding path after user click', async () => {
+    render(<ProfileCard profile={mockProfile} showEditButton={true} />);
+    const editProfileButton = screen.getByTestId('editProfileButton');
+    await userEvent.click(editProfileButton);
+    expect(mockPush).toHaveBeenCalledTimes(1);
+    expect(mockPush).toHaveBeenCalledWith('/onboarding?editing=true');
+  });
 });
--- components/ProfileCard/ProfileCard.tsx
+++ components/ProfileCard/ProfileCard.tsx
@@ -87,6 +87,7 @@ const ProfileCard = ({ profile, showEditButton = false }: ProfileCardProps) => {
         </div>
         {showEditButton && (
           <Button
+            data-testid='editProfileButton'
             className="bg-[#C5DBB2] text-black flex items-center gap-2 text-sm font-bold rounded-xl hover:bg-[#E4ECD9]"
             onClick={handleEditProfile}
           >
