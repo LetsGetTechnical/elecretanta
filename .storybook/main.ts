36d6406bbca55faa1ef635fe1da456e2638bd6e5
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-06-05 14:49:27 -0500

    chore: remove unused and redundant paths for stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,9 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: [
-    '../stories/*.mdx',
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   addons: [
     "@storybook/addon-essentials",
