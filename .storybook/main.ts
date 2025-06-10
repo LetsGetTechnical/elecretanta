a15420220e17cc3f7a4582b31477682af2b08d6b
Julianna Messineo <juliannamessineo@gmail.com>
2025-06-10 14:16:10 -0500

    fix: storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,17 +3,18 @@ import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: [
     '../stories/*.mdx',
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
   ],
   framework: {
-    name: "@storybook/nextjs",
-    options: {}
+    name: '@storybook/nextjs',
+    options: {},
   },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,12 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from '@storybook/react';
+import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
