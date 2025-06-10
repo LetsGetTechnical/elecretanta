2e483f67e92181e24833a016ac5ffd3e7f9601e6
Julianna Messineo <juliannamessineo@gmail.com>
2025-06-09 20:14:06 -0500

    chore: increase type safety with satisfies

--- components/FeedbackView/FeedbackView.test.tsx
+++ components/FeedbackView/FeedbackView.test.tsx
@@ -14,7 +14,7 @@ jest.mock('@/lib/generateAndUpdateNewGiftSuggestion', () => ({
     matchReasons: ['new test reason'],
     matchScore: 0.9,
     imageUrl: 'new-test.jpg',
-  }),
+  } satisfies GiftSuggestion),
 }));
 
 describe('FeedbackView', () => {
