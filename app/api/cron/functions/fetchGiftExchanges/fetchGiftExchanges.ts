29f6ea27b47c5cffa43f1ff780565381fd5d7ae2
Alex Appleget <137813659+alexappleget@users.noreply.github.com>
2025-06-20 11:43:05 -0500

    Alex/Implement automation for Gift Exchange Status - Phase 2/2 (#314)
    Co-authored-by: Shashi Lo <362527+shashilo@users.noreply.github.com>

---
+++ app/api/cron/functions/fetchGiftExchanges/fetchGiftExchanges.ts
@@ -0,0 +1,28 @@
+// Copyright (c) Gridiron Survivor.
+// Licensed under the MIT License.
+
+import { GiftExchange } from '@/app/types/giftExchange';
+import { SupabaseClient } from '@supabase/supabase-js';
+
+/**
+ * Fetches all gift exchanges in the database.
+ * @param {SupabaseClient} supabase - An instance of the Supabase client used to query the database.
+ * @returns {Promise<GiftExchange[]>} All gift exchanges in the form of an array of objects.
+ */
+export const fetchGiftExchanges = async ({
+  supabase,
+}: {
+  supabase: SupabaseClient;
+}): Promise<GiftExchange[]> => {
+  const { data, error } = await supabase.from('gift_exchanges').select('*');
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (data.length === 0) {
+    throw new Error('No gift exchanges found.');
+  }
+
+  return data;
+};
