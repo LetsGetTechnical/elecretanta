8bcfb30a376a8c099a9ff7695c444164dbb4d586
Hector <baseballhector@gmail.com>
2025-06-26 14:31:13 -0400

    chore: add 2 additional green background color options

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,6 +9,8 @@ const preview: Preview = {
       values: [
         { name: 'light', value: '#ffffff' },
         { name: 'dark', value: '#1a1a1a' },
+        { name: 'dark green', value: '#244b40' },
+        { name: 'green', value: '#395a50' },
         { name: 'gray', value: '#f0f0f0' },
         { name: 'blue', value: '#dbeafe' },
       ],
