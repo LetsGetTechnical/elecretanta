b508c9ea7a3b1d9cb61e2830c414dba32274dec2
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-06-30 13:26:18 -0600

    refactor: removed unneeded act and waitFor elements

--- components/UserDropdownMenu/UserDropdownMenu.test.tsx
+++ components/UserDropdownMenu/UserDropdownMenu.test.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Gridiron Survivor.
 // Licensed under the MIT License.
 
-import { render, screen, act, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import UserDropdownMenu from './UserDropdownMenu';
 
@@ -18,78 +18,55 @@ describe('UserDropdownMenu', () => {
   });
 
   it('displays Avatar on page load', async () => {
-    await act(async () => {
-      render(<UserDropdownMenu />);
-    });
-    await waitFor(() => {
-      expect(screen.getByTestId('avatar-body')).toBeInTheDocument();
-    });
+    render(<UserDropdownMenu />);
+
+    expect(screen.getByTestId('avatar-body')).toBeInTheDocument();
   });
 
   it('does not display dropdown menu contents on page load', async () => {
-    await act(async () => {
-      render(<UserDropdownMenu />);
-    });
-    await waitFor(() => {
-      expect(screen.queryByTestId('dropdown-menu-content')).toBeNull();
-    });
+    render(<UserDropdownMenu />);
+
+    expect(screen.queryByTestId('dropdown-menu-content')).toBeNull();
   });
 
   it('opens user dropdown menu when avatar is clicked', async () => {
-    await act(async () => {
-      render(<UserDropdownMenu />);
-    });
-    await waitFor(async () => {
-      await user.click(screen.getByTestId('dropdown-menu-trigger'));
-      expect(screen.getByTestId('dropdown-menu-content')).toBeInTheDocument();
-    });
+    render(<UserDropdownMenu />);
+
+    await user.click(screen.getByTestId('dropdown-menu-trigger'));
+    expect(screen.getByTestId('dropdown-menu-content')).toBeInTheDocument();
   });
 
   it('displays appropriate content when dropdown is open', async () => {
-    await act(async () => {
-      render(<UserDropdownMenu />);
-    });
-    await waitFor(async () => {
-      await user.click(screen.getByTestId('dropdown-menu-trigger'));
-      expect(screen.getByRole('link')).toHaveTextContent('My profile');
-      expect(
-        screen.getByRole('button', { name: 'Log out' }),
-      ).toBeInTheDocument();
-    });
+    render(<UserDropdownMenu />);
+
+    await user.click(screen.getByTestId('dropdown-menu-trigger'));
+    expect(screen.getByRole('link')).toHaveTextContent('My profile');
+    expect(screen.getByRole('button', { name: 'Log out' })).toBeInTheDocument();
   });
 
   it('allows user to navigate dropdown menu items with keyboard', async () => {
-    await act(async () => {
-      render(<UserDropdownMenu />);
-    });
-    await waitFor(async () => {
-      const trigger = screen.getByTestId('dropdown-menu-trigger');
-      trigger.focus();
-      await user.keyboard('[Space]');
-      expect(screen.getByTestId('my-profile-link')).toHaveFocus();
-      await user.keyboard('[ArrowDown]');
-      expect(screen.getByTestId('logout-button')).toHaveFocus();
-    });
+    render(<UserDropdownMenu />);
+
+    const trigger = screen.getByTestId('dropdown-menu-trigger');
+    trigger.focus();
+    await user.keyboard('[Space]');
+    expect(screen.getByTestId('my-profile-link')).toHaveFocus();
+    await user.keyboard('[ArrowDown]');
+    expect(screen.getByTestId('logout-button')).toHaveFocus();
   });
 
   it('navigates user to /profile page when My profile link is clicked', async () => {
-    await act(async () => {
-      render(<UserDropdownMenu />);
-    });
-    await waitFor(async () => {
-      await user.click(screen.getByTestId('dropdown-menu-trigger'));
-      expect(screen.getByRole('link')).toHaveAttribute('href', '/profile');
-    });
+    render(<UserDropdownMenu />);
+
+    await user.click(screen.getByTestId('dropdown-menu-trigger'));
+    expect(screen.getByRole('link')).toHaveAttribute('href', '/profile');
   });
 
   it('closes the dropdown menu after clicking on an item', async () => {
-    await act(async () => {
-      render(<UserDropdownMenu />);
-    });
-    await waitFor(async () => {
-      await user.click(screen.getByTestId('dropdown-menu-trigger'));
-      await user.click(screen.getByRole('link', { name: 'My profile' }));
-      expect(screen.queryByTestId('dropdown-menu-content')).toBeNull();
-    });
+    render(<UserDropdownMenu />);
+
+    await user.click(screen.getByTestId('dropdown-menu-trigger'));
+    await user.click(screen.getByRole('link', { name: 'My profile' }));
+    expect(screen.queryByTestId('dropdown-menu-content')).toBeNull();
   });
 });
