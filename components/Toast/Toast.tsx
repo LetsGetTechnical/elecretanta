3da74c0f2a6d7c155f81793e26b03da45ae32a98
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-09-08 13:19:59 -0500

    chore: import ToastVariants into Toast component

--- components/Toast/Toast.tsx
+++ components/Toast/Toast.tsx
@@ -7,23 +7,24 @@ import * as React from "react"
 import * as ToastPrimitives from "@radix-ui/react-toast"
 import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
+import { ToastVariants } from "./Toast.enum"
 
 const toastVariants = cva(
   "group pointer-events-auto relative flex w-full items-center justify-between space-x-2 overflow-hidden rounded-md border p-4 pr-6 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full",
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
-        error:
+        [ToastVariants.Default]: "border bg-background text-foreground",
+        [ToastVariants.Error]:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
-        warning: 
+        [ToastVariants.Warning]: 
           "border-warning bg-warning text-warning-foreground",
-        success:
+        [ToastVariants.Success]:
           "border-success bg-success text-success-foreground",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: ToastVariants.Default,
     },
   }
 )
