c96d40daa7cbe57248a95de8ebcbaf5a38e90ece
Yassahr <yassahr@gmail.com>
2025-09-10 17:25:52 -0400

    fix(testing): create test file

---
+++ components/Popover/Popover.test.tsx
@@ -0,0 +1,24 @@
+// Copyright (c) Gridiron Survivor.
+// Licensed under the MIT License.
+
+import { render, screen } from '@testing-library/react';
+import { Popover } from './popover';
+
+describe('Popover', () => {
+    it('renders the popover element', () => {
+        render(
+            <Popover>
+                <p>Hello</p>
+            </Popover>);
+        screen.debug()
+        
+        // const navLogoIcon = screen.getByTestId('nav-logo-icon');
+        // expect(navLogoIcon).toBeInTheDocument();
+        
+        // const navLogoText = screen.getByTestId('nav-logo-text');
+        // expect(navLogoText).toHaveTextContent('Elfgorithm');
+
+        // const navLogoLink = screen.getByTestId('nav-logo-link');
+        // expect(navLogoLink).toHaveAttribute('href', '/dashboard')
+    });
+})
--- components/Popover/popover.tsx
+++ components/Popover/popover.tsx
@@ -21,6 +21,7 @@ const PopoverContent = React.forwardRef<
   <PopoverPrimitive.Portal>
     <PopoverPrimitive.Content
       ref={ref}
+      // data-testid="popover"
       align={align}
       sideOffset={sideOffset}
       className={cn(
