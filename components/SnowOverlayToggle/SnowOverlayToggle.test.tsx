1be64b3ea729d548ec07d7509a9d3abdc02562fd
Zoobia! <160795349+zoobiasyyed@users.noreply.github.com>
2025-09-11 14:02:29 -0700

    Zoobia/212 unit test for snow overlay toggle (#381)
    Co-authored-by: Shashi Lo <362527+shashilo@users.noreply.github.com>

---
+++ components/SnowOverlayToggle/SnowOverlayToggle.test.tsx
@@ -0,0 +1,26 @@
+// Copyright (c) Gridiron Survivor.
+// Licensed under the MIT License.
+
+
+import { SnowOverlayToggle } from "./SnowOverlayToggle";
+import { render, screen } from '@testing-library/react';
+import { SnowOverlayProvider } from "@/providers/SnowOverlayProvider";
+import { userEvent } from "@storybook/test";
+
+
+describe('SnowOverlayToggle', () => {
+  it('Toggles the snowfall overlay', async ()=> {
+    render(
+      <SnowOverlayProvider>
+        <SnowOverlayToggle />
+      </SnowOverlayProvider>,
+    );
+    const sunIcon = screen.getByTestId('sun-icon');
+    expect(sunIcon).toBeInTheDocument();
+
+    await userEvent.click(screen.getByTestId('snow-overlay-toggle'));
+
+    const snowIcon = screen.getByTestId('snow-icon');
+    expect(snowIcon).toBeInTheDocument();
+  })
+})
