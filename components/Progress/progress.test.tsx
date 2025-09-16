8ab5ffe473d1f3514a38e5ccc83c52a23ff489d9
Tony Brierly <anthonylbrierly@gmail.com>
2025-09-16 15:56:19 -0700

    test & fix: Update unit tests based on comments. Address ESlint errors for Progress

--- components/Progress/progress.test.tsx
+++ components/Progress/progress.test.tsx
@@ -20,29 +20,29 @@ describe('Progress Component', () => {
     const progressElement = screen.getByRole('progressbar');
     expect(progressElement).toHaveClass('custom-class');
   });
-
+  
   it('applies indicatorClassName to the indicator', () => {
     render(<Progress indicatorClassName="indicator-custom-class" />);
     const indicatorElement = screen.getByTestId('progress-indicator');
     expect(indicatorElement).toHaveClass('indicator-custom-class');
   });
-
+  
   it('sets the indicator style based on value (0)', () => {
     render(<Progress value={0} />);
     const indicatorElement = screen.getByTestId('progress-indicator');
-    expect(indicatorElement).toHaveStyle('transform: translateX(-100%)');
+    expect(indicatorElement).toHaveAttribute("data-value", "0");
   });
-
+  
   it('sets the indicator style based on value (50)', () => {
     render(<Progress value={50} />);
     const indicatorElement = screen.getByTestId('progress-indicator');
-    expect(indicatorElement).toHaveStyle('transform: translateX(-50%)');
+    expect(indicatorElement).toHaveAttribute("data-value", "50");
   });
-
+  
   it('set the indicator style based on value (100)', () => {
     render(<Progress value={100} />);
     const indicatorElement = screen.getByTestId('progress-indicator');
-    expect(indicatorElement).toHaveStyle('transform: translateX(-0%)');
+    expect(indicatorElement).toHaveAttribute("data-value", "100");
   });
 
   it('spreads additional props to the root element', () => {
--- components/Progress/progress.tsx
+++ components/Progress/progress.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Gridiron Survivor.
+// Licensed under the MIT License.
+
 'use client';
 
 import * as React from 'react';
@@ -5,6 +8,14 @@ import * as ProgressPrimitive from '@radix-ui/react-progress';
 
 import { cn } from '@/lib/utils';
 
+/**
+ * A Progress Component with an indicator to represent the progress-to-overall ration.
+ * @param {object} [props] - Props for the Progress component.
+ * @param {string} [props.className] - Additional CSS classes for the Root element
+ * @param {number} [props.value] - Progress value, expected range: 0 - 100
+ * @param {string} [props.indicatorClassName] - Additional CSS classes for the Indicator element
+ * @returns {Progress} - returns Progress component.
+ */
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
@@ -21,6 +32,7 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       data-testid="progress-indicator"
+      data-value={value}
       className={cn(
         'h-full w-full flex-1 bg-[#E26969] transition-all rounded-full',
         indicatorClassName,
