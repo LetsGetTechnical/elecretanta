641fb0a5a39ebf88cf684451170ff224aff4eee7
Hector <baseballhector@gmail.com>
2025-09-19 18:01:09 -0400

    test: updated tests to account for affiliate tag and removed unneeded tests

--- components/GiftDetailsView/GiftDetailsView.test.tsx
+++ components/GiftDetailsView/GiftDetailsView.test.tsx
@@ -1,11 +1,9 @@
 // Copyright (c) Gridiron Survivor.
 // Licensed under the MIT License.
 
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import GiftDetailsView from './GiftDetailsView';
 import { describe } from 'node:test';
-import { userEvent } from '@storybook/test';
 
 const mockGift = {
   id: '1',
@@ -18,96 +16,75 @@ const mockGift = {
     'https://www.amazon.com/Cozzenity-Checkered-Blanket-Blankets-Lightweight/dp/B0F13991KJ/ref=sr_1_1_sspa?dib=eyJ2IjoiMSJ9.6wzR8Y2VhlZ6NsPFYW2cadhZFdeTIN7H6f_K3SufPMLJMToDPu0O_vCflzFycFVgQu9w8Xa2yC56dPFThjqnH5R3cM6i--ozxTqrjiTRLFj0OR_jh8wRe9Y0992_NPU9HrrjbKFeAWsFHKCcDxcGytcREvh7r2XZf-F-jcpqtBJFNhezM0lAhCO27_HYTgtb21GKie4DnzAWwESS488BC_7hi7FVKkDLJRJSmBIBro0WxlgebmNyTq6QqPcwQpM-o2Xa9hLm7DvYG0QSsRcR9QhK0aiRkQs6wLSW0YJHQBI.GiOlWv46xAPaV98XHUz3LzGTnXgnhW8lH6oq09Rz7Jk&dib_tag=se&hvadid=694526290027&hvdev=c&hvexpln=67&hvlocphy=9031568&hvnetw=g&hvocijid=18272751582845398260--&hvqmt=e&hvrand=18272751582845398260&hvtargid=kwd-460419430462&hydadcr=19233_13355042&keywords=amazon%2Bfuzzy%2Bblanket&mcid=5b4485e6f4883f9f84c099bcd937f120&qid=1747259407&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1',
 };
 
-const mockBadGift = {
+const mockGiftWithBadUrl = {
   id: '1',
   title: 'Blanket',
   price: '$29.99',
   description: 'fuzzy blanket',
   matchReasons: ['fits theme', 'highly rated'],
   matchScore: 0.98,
-  imageUrl:'bad_url',
-}
-
+  imageUrl: 'bad_url',
+};
 
 const mockHandleFeedback = jest.fn();
 
 describe('GiftDetailsView', () => {
-  //testing the gift and handleFeedback props
-  it('renders the component with correct props', () => {
+  it('renders the component with gift details', () => {
     render(
       <GiftDetailsView gift={mockGift} handleFeedback={mockHandleFeedback} />,
     );
+
+    const giftImage = screen.getByTestId('valid-image');
+    const matchScore = screen.getByTestId('valid-matchScore');
+    const price = screen.getByTestId('valid-price');
+    const title = screen.getByTestId('valid-title');
+    const description = screen.getByTestId('valid-description');
+    const giftReasons = screen.getByTestId('card-content').textContent;
+    const containsAllGiftReasons = mockGift.matchReasons.every((reason) =>
+      giftReasons.includes(reason),
+    );
+
+    expect(giftImage).toHaveAttribute('src', mockGift.imageUrl);
+    expect(giftImage).toHaveAttribute('alt', mockGift.title);
+    expect(matchScore).toHaveTextContent(mockGift.matchScore.toString());
+    expect(price).toHaveTextContent(mockGift.price);
+    expect(title).toHaveTextContent(mockGift.title);
+    expect(description).toHaveTextContent(mockGift.description);
+    expect(containsAllGiftReasons).toBe(true);
   });
 
-  //testing the Amazon URL
   it('generates the correct Amazon URL', async () => {
+    // Cloning the env variable
+    const original_NEXT_PUBLIC_AMAZON_AFFILIATE_TAG =
+      process.env.NEXT_PUBLIC_AMAZON_AFFILIATE_TAG;
+    process.env.NEXT_PUBLIC_AMAZON_AFFILIATE_TAG = 'test-tag';
+
     render(
       <GiftDetailsView gift={mockGift} handleFeedback={mockHandleFeedback} />,
     );
 
     const hrefLink = screen.getByRole('link', { name: /view/i });
-    await userEvent.click(hrefLink);
 
     expect(hrefLink).toHaveAttribute(
       'href',
-      'https://www.amazon.com/s?k=Blanket',
+      `https://www.amazon.com/s?k=${mockGift.title}&tag=${process.env.NEXT_PUBLIC_AMAZON_AFFILIATE_TAG}`,
     );
 
-  })
-
-  // testing conditional rendering for ImageURL
-  it('renders image if URL is present', () => {
-    render(
-      <GiftDetailsView gift={mockGift} handleFeedback={mockHandleFeedback} />,
-    );
-
-    const image = screen.getByTestId('valid-image');
-    expect(image).toBeInTheDocument();
-  })
-
-  //testing that Gift Icon appears if not valid image URL
-  //CAN NOT FIGURE THIS OUT
-  it('renders Gift Icon if URl is not valid', ()=> {
-     render(
-       <GiftDetailsView gift={mockBadGift} handleFeedback={mockHandleFeedback} />,
-     );
-
-     const giftIcon = screen.getByTestId('gift-icon');
-     expect(giftIcon).toBeInTheDocument();
-  })
-
-  it('renders a valid MatchScore and price', () => {
-    render(
-      <GiftDetailsView gift={mockGift} handleFeedback={mockHandleFeedback} />,
-    );
-
-    const matchScore = screen.getByTestId('valid-matchScore');
-    expect(matchScore).toBeInTheDocument();
-
-    const price = screen.getByTestId('valid-price');
-    expect(price).toBeInTheDocument();
-  })
-
-  it('renders a valid title and description', () => {
-    render(
-      <GiftDetailsView gift={mockGift} handleFeedback={mockHandleFeedback} />,
-    );
-
-    const title = screen.getByTestId('valid-title');
-    expect(title).toBeInTheDocument();
-
-    const description = screen.getByTestId('valid-description');
-    expect(description).toBeInTheDocument();
+    // Restoring the env variable
+    process.env.NEXT_PUBLIC_AMAZON_AFFILIATE_TAG =
+      original_NEXT_PUBLIC_AMAZON_AFFILIATE_TAG;
   });
 
-  it('correctly renders a list for match reasons', () => {
+  it('renders default Gift Icon on invalid image url', () => {
     render(
-      <GiftDetailsView gift={mockGift} handleFeedback={mockHandleFeedback} />,
+      <GiftDetailsView
+        gift={mockGiftWithBadUrl}
+        handleFeedback={mockHandleFeedback}
+      />,
     );
 
-    mockGift.matchReasons.forEach((reason) => {
-      expect(screen.getByText(reason)).toBeInTheDocument();
-    })
-  })
+    const giftIcon = screen.getByTestId('gift-icon');
 
+    expect(giftIcon).toBeInTheDocument();
+  });
 });
