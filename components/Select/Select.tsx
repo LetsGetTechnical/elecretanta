47b9802dfd039d9c88f9d17608249cea1b097b98
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2025-09-20 05:37:14 -0700

    fix:#280 address ESlint errors for Select component (#614)

--- components/Select/Select.tsx
+++ components/Select/Select.tsx
@@ -5,10 +5,29 @@
 
 import * as SelectPrimitive from '@radix-ui/react-select';
 
+/**
+ * Root component that contains all the parts of a select.
+ * Manages the selection state and handles all the interactions.
+ * @see {@link https://www.radix-ui.com/docs/primitives/components/select#root}
+ */
 const Select = SelectPrimitive.Root;
 
+/**
+ * Component used to group multiple items.
+ * Use in conjunction with Select.Label to ensure good accessibility via automatic labelling.
+ * @see {@link https://www.radix-ui.com/docs/primitives/components/select#group}
+ */
 const SelectGroup = SelectPrimitive.Group;
 
+/**
+ * Component that reflects the selected value.
+ * Appears in the trigger when a value is selected.
+ * By default the selected item's text will be rendered.
+ * If you require more control, you can instead control the select and pass your own children.
+ * It should not be styled to ensure correct positioning.
+ * An optional placeholder prop is also available for when the select has no value.
+ * @see {@link https://www.radix-ui.com/docs/primitives/components/select#value}
+ */
 const SelectValue = SelectPrimitive.Value;
 
 export { Select, SelectGroup, SelectValue };
