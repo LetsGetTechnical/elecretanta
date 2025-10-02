542269b3d55d2386f595e8912d8037d7c033c914
Yassahr <yassahr@gmail.com>
2025-10-02 17:32:33 -0400

    refactor: removed wrapper describe function and rename variable

--- components/Popover/Popover.test.tsx
+++ components/Popover/Popover.test.tsx
@@ -10,110 +10,107 @@ import {
 } from './popover';
 import userEvent from '@testing-library/user-event';
 
-describe('Popover Component', () => {
-  describe('Popover Component without data', () => {
-    beforeEach(() => {
-      render(
-        <Popover>
-          <PopoverTrigger></PopoverTrigger>
-          <PopoverContent></PopoverContent>
-          <PopoverAnchor />
-        </Popover>,
-      );
-    });
-  
-
-    it('renders the popover component and nested children components', () => {
-      const trigger = screen.getByTestId('popover-trigger');
-      const anchor = screen.getByTestId('popover-anchor');
-
-      expect(trigger).toBeInTheDocument();
-      expect(anchor).toBeInTheDocument();
-    });
-
-    it('popover opens when content is empty', async () => {
-      await userEvent.click(screen.getByTestId('popover-trigger'));
-      const content = screen.getByTestId('popover-content');
-
-      expect(content).toBeInTheDocument();
-    });
+describe('Popover Component without data', () => {
+  beforeEach(() => {
+    render(
+      <Popover>
+        <PopoverTrigger></PopoverTrigger>
+        <PopoverContent></PopoverContent>
+        <PopoverAnchor />
+      </Popover>,
+    );
   });
 
-  describe('Popover Component with data', () => {
-    beforeEach(() => {
-      render(
-        <Popover>
-          <PopoverTrigger>button</PopoverTrigger>
-          <PopoverContent>Content Here</PopoverContent>
-          <PopoverAnchor />
-        </Popover>,
-      );
-    })
-    it('renders content inside of trigger component', () => {
-      const trigger = screen.getByText('button');
+  it('renders the popover component and nested children components', () => {
+    const trigger = screen.getByTestId('popover-trigger');
+    const anchor = screen.getByTestId('popover-anchor');
 
-      expect(trigger).toBeInTheDocument();
-    });
-
-    it('when popover is closed, content is not visible', () => {
-      const content = screen.queryByText('Content Here');
-
-      expect(content).not.toBeInTheDocument();
-    });
-
-    it('content is visible when popover is opened', async () => {
-      const button = screen.getByText('button');
-      await userEvent.click(button);
-
-      const content = screen.queryByText('Content Here');
-      expect(content).toBeVisible();
-    });
-
-    it('content not visible when popover is opened then closed', async () => {
-      const button = screen.getByText('button');
-      //opens popover
-      await userEvent.click(button);
-
-      const content = screen.queryByText('Content Here');
-      expect(content).toBeVisible();
-
-      //closes popover
-      await userEvent.click(button);
-      expect(screen.queryByText('Content Here')).not.toBeInTheDocument();
-    });
+    expect(trigger).toBeInTheDocument();
+    expect(anchor).toBeInTheDocument();
   });
 
-  describe('Popover Component with attributes while popover is open', () => {
-    it('applies align attribute to content element', async () => {
-      render(
-        <Popover>
-          <PopoverTrigger>button</PopoverTrigger>
-          <PopoverContent>Content Here</PopoverContent>
-          <PopoverAnchor />
-        </Popover>,
-      );
-      const button = screen.getByText('button');
+  it('popover opens when content is empty', async () => {
+    await userEvent.click(screen.getByTestId('popover-trigger'));
+    const content = screen.getByTestId('popover-content');
 
-      await userEvent.click(button);
+    expect(content).toBeInTheDocument();
+  });
+});
 
-      const content = screen.queryByTestId('popover-content');
-      expect(content).toHaveAttribute('data-align', 'center');
-    });
+describe('Popover Component with data', () => {
+  beforeEach(() => {
+    render(
+      <Popover>
+        <PopoverTrigger>triggerContent</PopoverTrigger>
+        <PopoverContent>Content Here</PopoverContent>
+        <PopoverAnchor />
+      </Popover>,
+    );
+  });
+  it('renders content inside of trigger component', () => {
+    const trigger = screen.getByText('triggerContent');
 
-    it('applies align attribute with different value than default to content element', async () => {
-      render(
-        <Popover>
-          <PopoverTrigger>button</PopoverTrigger>
-          <PopoverContent align="start">Content Here</PopoverContent>
-          <PopoverAnchor />
-        </Popover>,
-      );
-      const button = screen.getByText('button');
+    expect(trigger).toBeInTheDocument();
+  });
 
-      await userEvent.click(button);
+  it('when popover is closed, content is not visible', () => {
+    const content = screen.queryByText('Content Here');
 
-      const content = screen.queryByTestId('popover-content');
-      expect(content).toHaveAttribute('data-align', 'start');
-    });
+    expect(content).not.toBeInTheDocument();
+  });
+
+  it('content is visible when popover is opened', async () => {
+    const triggerContent = screen.getByText('triggerContent');
+    await userEvent.click(triggerContent);
+
+    const content = screen.queryByText('Content Here');
+    expect(content).toBeVisible();
+  });
+
+  it('content not visible when popover is opened then closed', async () => {
+    const triggerContent = screen.getByText('triggerContent');
+    //opens popover
+    await userEvent.click(triggerContent);
+
+    const content = screen.queryByText('Content Here');
+    expect(content).toBeVisible();
+
+    //closes popover
+    await userEvent.click(triggerContent);
+    expect(screen.queryByText('Content Here')).not.toBeInTheDocument();
+  });
+});
+
+describe('Popover Component with attributes while popover is open', () => {
+  it('applies align attribute to content element', async () => {
+    render(
+      <Popover>
+        <PopoverTrigger>triggerContent</PopoverTrigger>
+        <PopoverContent>Content Here</PopoverContent>
+        <PopoverAnchor />
+      </Popover>,
+    );
+    const triggerContent = screen.getByText('triggerContent');
+
+    await userEvent.click(triggerContent);
+
+    const content = screen.queryByTestId('popover-content');
+    expect(content).toHaveAttribute('data-align', 'center');
+  });
+
+  it('applies align attribute with different value than default to content element', async () => {
+    render(
+      <Popover>
+        <PopoverTrigger>triggerContent</PopoverTrigger>
+        <PopoverContent align="start">Content Here</PopoverContent>
+        <PopoverAnchor />
+      </Popover>,
+    );
+    const triggerContent = screen.getByText('triggerContent');
+
+    await userEvent.click(triggerContent);
+
+    const content = screen.queryByTestId('popover-content');
+    expect(content).toHaveAttribute('data-align', 'start');
   });
 });
