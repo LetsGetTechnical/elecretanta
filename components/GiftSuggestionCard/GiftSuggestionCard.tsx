4beda0499fdd3f3398716c5dda1bfff3558e3928
Yassahr <yassahr@gmail.com>
2025-10-03 16:36:31 -0400

    fix: fix prop naming conv and add psuedo for test

--- components/GiftSuggestionCard/GiftSuggestionCard.tsx
+++ components/GiftSuggestionCard/GiftSuggestionCard.tsx
@@ -10,13 +10,13 @@ import { Profile } from '@/app/types/profile';
 
 /**
  * The Gift Suggestion Component for display gift or feedback
- * @param {object} giftSuggestionParams - The gift object all the params are associated with.
- * @param {IGiftSuggestion[]} giftSuggestionParams.allGiftSuggestions - the Array of all the gift suggestions.
- * @param {string} giftSuggestionParams.budget - the exchange budget for gift.
- * @param {IGiftSuggestion} giftSuggestionParams.gift - the gift suggestion.
- * @param {number} giftSuggestionParams.index - the index of the gift.
- * @param {IGiftSuggestion | number} giftSuggestionParams.onGiftUpdate - the updated value of index and gift suggestion.
- * @param {Profile | null} giftSuggestionParams.recipient - The user information for gift recipient.
+ * @param {object} props - The gift object all the params are associated with.
+ * @param {IGiftSuggestion[]} props.allGiftSuggestions - the Array of all the gift suggestions.
+ * @param {string} props.budget - the exchange budget for gift.
+ * @param {IGiftSuggestion} props.gift - the gift suggestion.
+ * @param {number} props.index - the index of the gift.
+ * @param {IGiftSuggestion | number} props.onGiftUpdate - the updated value of index and gift suggestion.
+ * @param {Profile | null} props.recipient - The user information for gift recipient.
  * @returns {JSX.Element} - The gift suggestion card component.
  */
 const GiftSuggestionCard = ({
@@ -41,13 +41,13 @@ const GiftSuggestionCard = ({
    * @param {IGiftSuggestion} updatedGift - the updated gift value
    * @returns {Promise<void>} - sets state and updated gift values
    */
-  const handleGiftUpdate = async (
+  const handleGiftUpdate = async ( //onhandle
     updatedGift: IGiftSuggestion,
   ): Promise<void> => {
     onGiftUpdate(updatedGift, index);
     setIsShowingFeedback(false);
   };
-
+//handleGiftUpdate
   return (
     <Card className="bg-giftSuggestionsCardBackground h-100 w-80 flex flex-col justify-between m-5">
       {isShowingFeedback ? (
