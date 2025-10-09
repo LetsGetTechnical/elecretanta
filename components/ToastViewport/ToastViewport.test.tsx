d583365bc08bf94606d16205ce95dd01af20869a
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-10-09 18:41:25 -0500

    refactor: eslint formatting and style change

--- components/ToastViewport/ToastViewport.test.tsx
+++ components/ToastViewport/ToastViewport.test.tsx
@@ -2,37 +2,38 @@ import { ToastProvider } from '@radix-ui/react-toast';
 import { ToastViewport } from './ToastViewport';
 import { render, screen } from '@testing-library/react';
 
-// move toastViewport to the top since i call it every time? 
-
-const renderViewport = (props = {}) => (
-    render(
-         <ToastProvider>
-                <ToastViewport {...props} />
-            </ToastProvider>
-    )
-)
+const renderViewport = (props = {}) =>
+  render(
+    <ToastProvider>
+      <ToastViewport {...props} />
+    </ToastProvider>,
+  );
 
 describe('ToastViewport', () => {
-    it('renders the toast viewport', () => {
-        renderViewport()
-        expect(screen.getByTestId('toastViewport')).toBeInTheDocument();
-    })
+  it('renders the toast viewport', () => {
+    renderViewport();
+    expect(screen.getByTestId('toastViewport')).toBeInTheDocument();
+  });
 
-    it('applies default classes', () => {
-        renderViewport()
-        const toastViewport = screen.getByTestId('toastViewport');
-        expect(toastViewport).toHaveClass('fixed top-0 z-[100]', 'max-h-screen', 'sm:bottom-0')
-    })
+  it('applies default classes', () => {
+    renderViewport();
+    const toastViewport = screen.getByTestId('toastViewport');
+    expect(toastViewport).toHaveClass(
+      'fixed top-0 z-[100]',
+      'max-h-screen',
+      'sm:bottom-0',
+    );
+  });
 
-    it('applies custom classNames', () => {
-        renderViewport({ className: "custom-class" });
-        const toastViewport = screen.getByTestId('toastViewport');
-        expect(toastViewport).toHaveClass('custom-class');
-    })
+  it('applies custom classNames', () => {
+    renderViewport({ className: 'custom-class' });
+    const toastViewport = screen.getByTestId('toastViewport');
+    expect(toastViewport).toHaveClass('custom-class');
+  });
 
-    it('passes custom attributes', () => {
-        renderViewport({ "data-custom-attribute": "testValue" });
-        const toastViewport = screen.getByTestId('toastViewport');
-        expect(toastViewport).toHaveAttribute('data-custom-attribute', 'testValue')
-    })
-})
+  it('passes custom attributes', () => {
+    renderViewport({ 'data-custom-attribute': 'testValue' });
+    const toastViewport = screen.getByTestId('toastViewport');
+    expect(toastViewport).toHaveAttribute('data-custom-attribute', 'testValue');
+  });
+});
