93ade3a6bbc2e3547ac98dab7da9884235df0503
Alex Appleget <alexappleget2014@gmail.com>
2025-10-08 19:27:42 -0500

    Fix: made past dates unselectable for Calendar components in create-group and edit-group

---
+++ app/create-group-page/page.test.tsx
@@ -0,0 +1,27 @@
+import { Calendar } from '@/components/Calendar/calendar';
+import { render } from '@testing-library/react';
+
+describe('Calendar', () => {
+  it('disables past dates before today', () => {
+    const currentDate = new Date('2025-10-08T00:00:00Z');
+    const disabledFunction = (date: Date) => date < currentDate;
+
+    render(
+      <Calendar
+        mode="single"
+        selected={currentDate}
+        onSelect={() => {}}
+        disabled={disabledFunction}
+        initialFocus
+      />,
+    );
+
+    const yesterday = new Date('2025-10-07T00:00:00Z');
+    const today = new Date('2025-10-08T00:00:00Z');
+    const tomorrow = new Date('2025-10-09T00:00:00Z');
+
+    expect(disabledFunction(yesterday)).toBe(true);
+    expect(disabledFunction(today)).toBe(false);
+    expect(disabledFunction(tomorrow)).toBe(false);
+  });
+});
--- app/gift-exchanges/[id]/edit/page.tsx
+++ app/gift-exchanges/[id]/edit/page.tsx
@@ -336,9 +336,7 @@ export default function CreateGroupPage() {
                           mode="single"
                           selected={field.value}
                           onSelect={field.onChange}
-                          // disabled={(date) =>
-                          //   date > new Date() || date < new Date("1900-01-01")
-                          // }
+                          disabled={(date) => date < new Date()}
                           initialFocus
                         />
                       </PopoverContent>
