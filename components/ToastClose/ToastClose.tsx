487f574d3987ac15432b0d1384a963b2d8ce8d07
Lillian Nguyen <144195411+lillian-nguyen@users.noreply.github.com>
2025-10-10 09:17:46 -0500

    Feat #588: Toast Component (#598)

---
+++ components/ToastClose/ToastClose.tsx
@@ -0,0 +1,35 @@
+// Copyright (c) Gridiron Survivor.
+// Licensed under the MIT License.
+
+import React from 'react';
+import { X } from 'lucide-react';
+import * as ToastPrimitives from '@radix-ui/react-toast';
+import { cn } from '@/lib/utils';
+
+const ToastClose = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitives.Close>,
+  React.ComponentPropsWithoutRef<typeof ToastPrimitives.Close>
+>(({ className, ...props }, ref) => (
+  <ToastPrimitives.Close
+    ref={ref}
+    className={cn(
+      'absolute right-1 top-1 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-1 group-hover:opacity-100',
+
+      'group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600',
+
+      'group-[.success]:text-white/70 group-[.success]:hover:text-white group-[.success]:focus:ring-white/50',
+
+      'group-[.warning]:text-gray-800/70 group-[.warning]:hover:text-gray-900 group-[.warning]:focus:ring-gray-900/50',
+      className,
+    )}
+    toast-close=""
+    aria-label="Close"
+    data-testid="toastClose"
+    {...props}
+  >
+    <X className="h-4 w-4" />
+  </ToastPrimitives.Close>
+));
+ToastClose.displayName = ToastPrimitives.Close.displayName;
+
+export { ToastClose };
