6e7a6e3612fa0c05cd9da9c196351784e639edec
Hector <baseballhector@gmail.com>
2025-10-19 17:30:59 -0400

    test: initial tests for InviteCard

---
+++ components/InviteCard/InviteCard.test.tsx
@@ -0,0 +1,65 @@
+import { InviteCard } from './InviteCard';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('InviteCard', () => {
+  const mockInviteUrl = 'https://example.com/invite-123';
+  const originalClipboard: Clipboard = global.navigator.clipboard;
+
+  // Mocks the window location
+  beforeEach(() => {
+    Object.defineProperty(window, 'location', {
+      value: { href: mockInviteUrl },
+    });
+  });
+
+  beforeEach(() => {
+    // Mocking the navigator clipboard
+    Object.defineProperty(navigator, 'clipboard', {
+      value: {
+        writeText: jest.fn(),
+      },
+      configurable: true,
+    });
+  });
+
+  afterEach(() => {
+    // Restores the original navigator clipboard
+    Object.defineProperty(navigator, 'clipboard', {
+      value: originalClipboard,
+      configurable: true,
+    });
+  });
+
+  it('renders the InviteCard with input and link', () => {
+    render(<InviteCard />);
+
+    const inputElement = screen.getByRole('textbox');
+    const buttonElement = screen.getByRole('button', {
+      name: /Copy invite link/i,
+    });
+
+    expect(buttonElement).toBeInTheDocument();
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it('renders the input with correct invite link', () => {
+    render(<InviteCard />);
+
+    const inputElement = screen.getByRole('textbox');
+
+    expect(inputElement).toHaveAttribute('value', mockInviteUrl);
+  });
+
+  it('copies correct invite link to clipboard', async () => {
+    render(<InviteCard />);
+
+    const buttonElement = screen.getByRole('button', {
+      name: /Copy invite link/i,
+    });
+
+    await userEvent.click(buttonElement);
+
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(mockInviteUrl);
+  });
+});
