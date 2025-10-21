217be898d5c13d35e98fa6d0d19f9eae0b9a4c0c
Alex Appleget <137813659+alexappleget@users.noreply.github.com>
2025-10-21 11:31:16 -0500

    Fix: made past dates unselectable for Calendar components in create-group and edit-group (#630)

---
+++ app/create-group-page/page.test.tsx
@@ -0,0 +1,27 @@
+import { Calendar } from '@/components/Calendar/calendar';
+import { render, screen } from '@testing-library/react';
+
+describe('Calendar component in create group page', () => {
+  it('disables past dates correctly', () => {
+    const currentDate = new Date('2025-10-08T00:00:00Z');
+
+    render(
+      <Calendar
+        mode="single"
+        selected={currentDate}
+        onSelect={() => {}}
+        disabled={[{ before: currentDate }]}
+        initialFocus
+      />,
+    );
+
+    const pastDate = screen.getByText('5');
+    expect(pastDate).toBeDisabled();
+
+    const today = screen.getByText('8');
+    expect(today).not.toBeDisabled();
+
+    const tomorrow = screen.getByText('9');
+    expect(tomorrow).not.toBeDisabled();
+  });
+});
