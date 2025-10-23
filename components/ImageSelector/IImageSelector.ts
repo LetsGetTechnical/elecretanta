272d3b6c72e3bb9175e58c8b23713c1fd17f782e
Tony Brierly <anthonylbrierly@gmail.com>
2025-10-22 17:29:00 -0700

    Extract Unsplash loader function

--- components/ImageSelector/IImageSelector.ts
+++ components/ImageSelector/IImageSelector.ts
@@ -1,18 +1,12 @@
 // Copyright (c) Gridiron Survivor.
 // Licensed under the MIT License.
 
+import { ImageLoader } from 'next/image';
+
 export interface GroupImage {
   id: string;
   title: string;
   src: string;
-  loader?: ({
-    src,
-    width,
-    quality,
-  }: {
-    src: string;
-    width: number;
-    quality?: number;
-  }) => string;
+  loader?: ImageLoader;
   alt: string;
 }
--- components/ImageSelector/ImageSelector.tsx
+++ components/ImageSelector/ImageSelector.tsx
@@ -1,35 +1,35 @@
 'use client';
 
-import Image from 'next/image';
+import Image, { ImageLoader, ImageLoaderProps } from 'next/image';
 import { Card } from '@/components/Card/Card';
 import { RadioGroup } from '@/components/RadioGroup/RadioGroup';
 import { RadioGroupItem } from '@/components/RadioGroup/RadioGroupItem';
 import { Label } from '@/components/Label/Label';
 import { GroupImage } from './IImageSelector';
 
+const unsplashLoader: ImageLoader = ({ src, width, quality }) =>
+  `${src}${src.includes('?') ? '&' : '?'}w=${width}&q=${quality || 75}"}`;
+
 export const GROUP_IMAGES: GroupImage[] = [
   {
     id: '1',
     title: 'Old-timey parcels',
     src: 'https://images.unsplash.com/photo-1480632563560-30f503c09195?ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGNocmlzdG1hcyUyMGNoYXJhY3RlcnxlbnwwfHwwfHx8MA%3D%3D',
-    loader: ({ src, width, quality }) =>
-      `${src}${src.includes("?") ? "&" : "?"}w=${width}&q=${quality || 75}"}`,
+    loader: unsplashLoader,
     alt: 'Old-fashioned wrapped Christmas gifts.',
   },
   {
     id: '2',
     title: 'Gifts under the tree',
     src: 'https://plus.unsplash.com/premium_photo-1681426549371-f85391e73e60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGhvbGlkYXklMjBjaGFyYWN0ZXJzfGVufDB8fDB8fHww',
-    loader: ({ src, width, quality }) =>
-      `${src}${src.includes("?") ? "&" : "?"}w=${width}&q=${quality || 75}"}`,
+    loader: unsplashLoader,
     alt: 'Bright cartoony characters with gifts under a tree.',
   },
   {
     id: '3',
     title: 'Opening a gift box',
     src: 'https://plus.unsplash.com/premium_photo-1669242712308-4b0aef7166da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2hyaXN0bWFzJTIwY2hhcmFjdGVyfGVufDB8fDB8fHww',
-    loader: ({ src, width, quality }) =>
-      `${src}${src.includes("?") ? "&" : "?"}w=${width}&q=${quality || 75}"}`,
+    loader: unsplashLoader,
     alt: 'Illustration of a gift box with the lid swinging up.',
   },
 ];
