8b4ee9a535b9dd8b3deb9eb44c3f6611065621ad
jefische <jefische86@gmail.com>
2025-10-27 15:40:36 -0500

    Update to test drawing and exchange date elements

--- components/JourneyCard/JourneyCard.test.tsx
+++ components/JourneyCard/JourneyCard.test.tsx
@@ -1,16 +1,23 @@
 import { JourneyCard } from './JourneyCard';
 import { render, screen } from '@testing-library/react';
+import { formatDate } from '@/lib/utils';
 
 describe('JourneyCard Component', () => {
 
-  it('Renders the component with proper supplied dates', () => {
+  it('Renders the component given serialized ISO date string values', () => {
     render(<JourneyCard drawingDate='2025-10-31T05:00:00+00:00' exchangeDate='2025-11-27T06:00:00+00:00'/>);
 
-    screen.debug();
     const journeyCardElement = screen.getByTestId('journey-card');
     expect(journeyCardElement).toBeInTheDocument();
+    
+    const drawingDateElement = screen.getByText(/we'll review who you're gifting to/)
+    const drawingDateValue = formatDate('2025-10-31T05:00:00+00:00');
+    expect(drawingDateElement).toHaveTextContent(drawingDateValue);
+
+    const exchangeDateElement = screen.getByText(/Bring your wrapped gift to exchange on/)
+    const exchangeDateValue = formatDate('2025-11-27T06:00:00+00:00');
+    expect(exchangeDateElement).toHaveTextContent(exchangeDateValue);
 
   });
-
 
 });
