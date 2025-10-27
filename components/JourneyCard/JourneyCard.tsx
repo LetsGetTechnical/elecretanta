68cdc182dc1d5062ae8628dd62afce0a157c3ee4
jefische <jefische86@gmail.com>
2025-10-27 10:22:06 -0500

    addressing JSdoc and array key issues

--- components/JourneyCard/JourneyCard.tsx
+++ components/JourneyCard/JourneyCard.tsx
@@ -9,18 +9,27 @@ import {
 } from '@/components/Card/Card';
 import { formatDate } from '@/lib/utils';
 import { Gift, UserCircle, Sparkles, PartyPopper } from 'lucide-react';
+import { JSX } from 'react';
 
 interface JourneyCardProps {
   drawingDate: string;
   exchangeDate: string;
 }
 
+/**
+ * JourneyCard component
+ * @param {JourneyCardProps} props - Component props.
+ * @param {string} props.drawingDate - Date string representing the day of the randomized drawing.
+ * @param {string} props.exchangeDate - Date string representing the day of the gift exchange.
+ * @returns {JSX.Element} - The rendered JourneyCard element.
+ */
 export const JourneyCard = ({
   drawingDate,
   exchangeDate,
-}: JourneyCardProps) => {
+}: JourneyCardProps): JSX.Element => {
   const journeySteps = [
     {
+      id: 1,
       icon: <Gift className="text-groupCardGreen" />,
       title: 'Secret Match',
       description: `On ${formatDate(
@@ -28,18 +37,21 @@ export const JourneyCard = ({
       )}, we'll review who you're gifting to`,
     },
     {
+      id: 2,
       icon: <UserCircle className="text-groupCardGreen" />,
       title: 'Profile Insights',
       description:
         'See their interests and preferences to guide your gift choice. Update your own profile anytime to help your Secret Santa',
     },
     {
+      id: 3,
       icon: <Sparkles className="text-groupCardGreen" />,
       title: 'Smart Gift Ideas',
       description:
         'Our AI suggests personalized gifts based on their interests. Your feedback helps find the perfect match.',
     },
     {
+      id: 4,
       icon: <PartyPopper className="text-groupCardGreen" />,
       title: 'Exchange Time',
       description: `Bring your wrapped gift to exchange on ${formatDate(
@@ -57,8 +69,8 @@ export const JourneyCard = ({
       </CardHeader>
       <CardContent className="pt-4">
         <div className="flex flex-col gap-6">
-          {journeySteps.map((step, index) => (
-            <div key={index} className="flex gap-4 items-center">
+          {journeySteps.map((step) => (
+            <div key={step.id} className="flex gap-4 items-center">
               <div className="bg-white rounded-full w-10 h-10 flex items-center justify-center">
                 {step.icon}
               </div>
