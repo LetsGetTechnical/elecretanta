c15678cb0bb5527208daed35895b7feb6002abaa
Tony Brierly <anthonylbrierly@gmail.com>
2025-10-30 14:37:18 -0700

    Restore working directory

--- app/gift-exchanges/[id]/page.test.tsx
+++ app/gift-exchanges/[id]/page.test.tsx
@@ -6,6 +6,7 @@ const mockGiftExchangeData = {
   id: '123',
   name: 'Test Exchange',
   budget: '50',
+  status: 'pending',
 };
 
 const mockMembers = [
@@ -37,138 +38,50 @@ jest.mock('next/navigation', () => ({
 
 jest.mock('@/context/AuthContextProvider');
 
-describe('GiftExchangePage', () => {
-  describe('GiftExchangePage Warning Modal', () => {
-    beforeEach(() => {
-      jest.resetAllMocks();
+describe('GiftExchangePage Warning Modal', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
 
-      global.fetch = jest
-        .fn()
-        .mockResolvedValueOnce({
-          json: async () => ({ ...mockGiftExchangeData, status: 'pending' }),
-        })
-        .mockResolvedValueOnce({
-          json: async () => mockMembers,
-        })
-        .mockResolvedValueOnce({
-          json: async () => mockGiftSuggestions,
-        });
-    });
-
-    it('displays the WarningModal with the join button for a logged-in user who is not a member of a pending group', async () => {
-      (useAuthContext as jest.Mock).mockReturnValue({
-        session: {
-          user: {
-            id: 'not-a-member',
-          },
-        },
+    global.fetch = jest
+      .fn()
+      .mockResolvedValueOnce({
+        json: async () => mockGiftExchangeData,
+      })
+      .mockResolvedValueOnce({
+        json: async () => mockMembers,
+      })
+      .mockResolvedValueOnce({
+        json: async () => mockGiftSuggestions,
       });
-
-      render(<GiftExchangePage />);
-
-      const warningModal = await screen.findByTestId('warning-modal');
-      const joinButton = screen.getByTestId('join-button');
-
-      expect(warningModal).toBeInTheDocument();
-      expect(joinButton).toBeInTheDocument();
-    });
-
-    it('displays the WarningModal with a Google sign-in button for users who are not signed in and not members of a pending group', async () => {
-      (useAuthContext as jest.Mock).mockReturnValue({ session: null });
-
-      render(<GiftExchangePage />);
-
-      const warningModal = await screen.findByTestId('warning-modal');
-      const googleButton = screen.getByTestId('google-button');
-
-      expect(warningModal).toBeInTheDocument();
-      expect(googleButton).toBeInTheDocument();
-    });
   });
 
-  describe('GiftExchangePage Redirect Cards', () => {
-    beforeEach(() => {
-      jest.resetAllMocks();
-    });
-
-    it('displays a BAD LINK Redirect Card if the exchange is not valid', async () => {
-      (useAuthContext as jest.Mock).mockReturnValue({
-        session: { user: mockMembers[0] },
-      });
-
-      global.fetch = jest
-        .fn()
-        .mockResolvedValueOnce({
-          json: async () => ({ error: '' }),
-        })
-        .mockResolvedValueOnce({
-          json: async () => ({ error: '' }),
-        })
-        .mockResolvedValueOnce({
-          json: async () => ({ error: '' }),
-        });
-
-      render(<GiftExchangePage />);
-
-      const card = await screen.findByTestId('card-title');
-      expect(card).toHaveTextContent(/bad link/i);
-
-      const homeButton = screen.getByRole('link');
-      expect(homeButton).toHaveTextContent(/home/i);
-    });
-
-    it('displays a LOG IN Redirect Card if a user is not logged in and the group is not pending', async () => {
-      (useAuthContext as jest.Mock).mockReturnValue({ session: null });
-
-      global.fetch = jest
-        .fn()
-        .mockResolvedValueOnce({
-          json: async () => ({ ...mockGiftExchangeData, status: 'active' }),
-        })
-        .mockResolvedValueOnce({
-          json: async () => mockMembers,
-        })
-        .mockResolvedValueOnce({
-          json: async () => mockGiftSuggestions,
-        });
-
-      render(<GiftExchangePage />);
-
-      const card = await screen.findByTestId('card-title');
-      expect(card).toHaveTextContent(/log in/i);
-
-      const homeButton = screen.getByRole('link');
-      expect(homeButton).toHaveTextContent(/home/i);
-    });
-
-    it('displays an EXPIRED LINK Redirect Card if a user is logged in but not a member of the group and the group is not pending', async () => {
-      (useAuthContext as jest.Mock).mockReturnValue({
-        session: {
-          user: {
-            id: 'not-a-member',
-          },
+  it('displays the WarningModal with the join button for a logged-in user who is not a member of a pending group', async () => {
+    (useAuthContext as jest.Mock).mockReturnValue({
+      session: {
+        user: {
+          id: 'not-a-member',
         },
-      });
-
-      global.fetch = jest
-        .fn()
-        .mockResolvedValueOnce({
-          json: async () => ({ ...mockGiftExchangeData, status: 'active' }),
-        })
-        .mockResolvedValueOnce({
-          json: async () => mockMembers,
-        })
-        .mockResolvedValueOnce({
-          json: async () => mockGiftSuggestions,
-        });
-
-      render(<GiftExchangePage />);
-
-      const card = await screen.findByTestId('card-title');
-      expect(card).toHaveTextContent(/expired link/i);
-
-      const homeButton = screen.getByRole('link');
-      expect(homeButton).toHaveTextContent(/home/i);
+      },
     });
+
+    render(<GiftExchangePage />);
+
+    const warningModal = await screen.findByTestId('warning-modal');
+    const joinButton = screen.getByTestId('join-button');
+
+    expect(warningModal).toBeInTheDocument();
+    expect(joinButton).toBeInTheDocument();
+  });
+
+  it('displays the WarningModal with a Google sign-in button for users who are not signed in and not members of a pending group', async () => {
+    (useAuthContext as jest.Mock).mockReturnValue({ session: null });
+
+    render(<GiftExchangePage />);
+
+    const warningModal = await screen.findByTestId('warning-modal');
+    const googleButton = screen.getByTestId('google-button');
+
+    expect(warningModal).toBeInTheDocument();
+    expect(googleButton).toBeInTheDocument();
   });
 });
--- app/gift-exchanges/[id]/page.tsx
+++ app/gift-exchanges/[id]/page.tsx
@@ -4,20 +4,19 @@ import { GiftExchange } from '@/app/types/giftExchange';
 import { useParams } from 'next/navigation';
 import { useEffect, useState, useCallback } from 'react';
 import { GiftExchangeHeader } from '@/components/GiftExchangeHeader/GiftExchangeHeader';
-import { JourneyCard } from '@/components/JourneyCard/JourneyCard';
-import { MembersList } from '@/components/MembersList/MembersList';
-import { InviteCard } from '@/components/InviteCard/InviteCard';
-import { LoadingSkeleton } from '@/components/LoadingSkeleton/LoadingSkeleton';
+import { JourneyCard } from '../../../components/JourneyCard/JourneyCard';
+import { MembersList } from '../../../components/MembersList/MembersList';
+import { InviteCard } from '../../../components/InviteCard/InviteCard';
+import { LoadingSkeleton } from '../../../components/LoadingSkeleton/LoadingSkeleton';
 import { GiftExchangeMember } from '@/app/types/giftExchangeMember';
-import WarningModal from '@/components/WarningModal/WarningModal';
-import { CompletedExchangeCard } from '@/components/CompletedExchangeCard/CompletedExchangeCard';
+import WarningModal from '../../../components/WarningModal/WarningModal';
+import { CompletedExchangeCard } from '../../../components/CompletedExchangeCard/CompletedExchangeCard';
 import { Profile } from '@/app/types/profile';
 import ProfileCard from '@/components/ProfileCard/ProfileCard';
 import GiftSuggestionCard from '@/components/GiftSuggestionCard/GiftSuggestionCard';
 import { IGiftSuggestion } from '@/app/types/giftSuggestion';
 import { useAuthContext } from '@/context/AuthContextProvider';
 import { WaitingForSuggestions } from './WaitingForSuggestions/WaitingForSuggestions';
-import RedirectCard from '@/components/RedirectCard/RedirectCard';
 
 export default function GiftExchangePage() {
   const { id } = useParams();
@@ -102,71 +101,9 @@ export default function GiftExchangePage() {
     }
   };
 
-  const isValidGiftExchange = !!giftExchangeData?.id
-
   if (isLoading) {
     return <LoadingSkeleton statsCount={4} cardItemCount={10} />;
   }
-
-  if (!isValidGiftExchange) {
-    return (
-      <main className="min-h-screen-minus-20 flex justify-center">
-        <section className="max-w-xs mt-12">
-          <RedirectCard 
-            title="Bad Link" 
-            description='This link may be invalid or expired. Please double-check your
-                invitation link.'
-            buttonHref='/'
-            buttonLabel='Home'
-          />
-        </section>
-      </main>
-    );
-  }
-
-  if (!session && giftExchangeData.status !== 'pending') {
-    return (
-      <main className="min-h-screen-minus-20 flex justify-center">
-        <section className="max-w-xs mt-12">
-          <RedirectCard 
-            title="Log In" 
-            description='Please log in to visit this page.'
-            buttonHref='/'
-            buttonLabel='Home'
-          />
-        </section>
-      </main>
-    );
-  }
-  
-  if (!isUserAMember && giftExchangeData.status !== 'pending') {
-    return (
-      <main className="min-h-screen-minus-20 flex justify-center">
-        <section className="max-w-xs mt-12">
-          <RedirectCard 
-            title="Expired Link" 
-            description='Your invitation link may be outdated.'
-            buttonHref='/'
-            buttonLabel='Home'
-          />
-        </section>
-      </main>
-    );
-  }
-
-
-  if (giftExchangeData.status === 'pending' && (!session || !isUserAMember) ) {
-    return (
-      <main className="min-h-screen-minus-20">
-        <WarningModal
-          giftExchangeData={giftExchangeData}
-          members={giftExchangeMembers}
-          updateGiftExchangeMembers={updateGiftExchangeMembers}
-        />
-      </main>
-    );
-  }
-
 
   const renderContent = () => {
     switch (giftExchangeData.status) {
@@ -194,7 +131,7 @@ export default function GiftExchangePage() {
               <h1 className="font-bold">Gift Suggestions</h1>
               {giftSuggestions.length === 0 && <WaitingForSuggestions />}
 
-              {giftSuggestions.length > 0 && (
+              {giftSuggestions.length !== 0 && (
                 <div className="flex flex-row flex-wrap">
                   {giftSuggestions.map((gift, index) => (
                     <GiftSuggestionCard
@@ -223,6 +160,15 @@ export default function GiftExchangePage() {
 
   return (
     <main className="min-h-screen-minus-20">
+      {isUserAMember === false &&
+        !isLoading &&
+        giftExchangeData.status === 'pending' && (
+          <WarningModal
+            giftExchangeData={giftExchangeData}
+            members={giftExchangeMembers}
+            updateGiftExchangeMembers={updateGiftExchangeMembers}
+          />
+        )}
       <section className="mx-auto flex flex-col gap-4 px-4 md:px-16 lg:px-32 xl:px-52 pt-12 text-primary-foreground">
         <GiftExchangeHeader
           giftExchangeData={giftExchangeData}
