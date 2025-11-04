1bb3acdca1382aaa4964d36e3e469aada293e0f3
Hector <baseballhector@gmail.com>
2025-11-04 17:36:56 -0500

    test: adds test to correctly display gift exchange pairs

--- components/CompletedExchangeCard/CompletedExchangeCard.test.tsx
+++ components/CompletedExchangeCard/CompletedExchangeCard.test.tsx
@@ -118,4 +118,51 @@ describe('CompletedExchangeCard', () => {
     const members = screen.getAllByTestId(/^member-\d+$/);
     expect(members).toHaveLength(mockMembers.length);
   });
+
+  it('renders the correct member pairings', () => {
+    render(<CompletedExchangeCard members={mockMembers} />);
+
+    const givingMembers = screen.getAllByTestId(/^member-\d+$/);
+    const recipients = screen.getAllByTestId(/^recipient-\d+$/);
+    const avatars = screen.getAllByTestId(/avatar-image/i);
+
+    // Matching givers to recipients by name
+    expect(givingMembers[0]).toHaveTextContent('John Doe');
+    expect(recipients[0]).toHaveTextContent('Jane Smith');
+
+    expect(givingMembers[1]).toHaveTextContent('Jane Smith');
+    expect(recipients[1]).toHaveTextContent('Robert Joe');
+
+    expect(givingMembers[2]).toHaveTextContent('Robert Joe');
+    expect(recipients[2]).toHaveTextContent('John Doe');
+
+    // Matching givers to recipients by avatars
+    // John => Jane
+    expect(avatars[0]).toHaveAttribute(
+      'src',
+      expect.stringMatching(/john.png/i),
+    );
+    expect(avatars[1]).toHaveAttribute(
+      'src',
+      expect.stringMatching(/jane.png/i),
+    );
+    // Jane => Robert
+    expect(avatars[2]).toHaveAttribute(
+      'src',
+      expect.stringMatching(/jane.png/i),
+    );
+    expect(avatars[3]).toHaveAttribute(
+      'src',
+      expect.stringMatching(/robert.png/i),
+    );
+    // Robert => John
+    expect(avatars[4]).toHaveAttribute(
+      'src',
+      expect.stringMatching(/robert.png/i),
+    );
+    expect(avatars[5]).toHaveAttribute(
+      'src',
+      expect.stringMatching(/john.png/i),
+    );
+  });
 });
