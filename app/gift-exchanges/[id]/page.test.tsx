a1a689221d29a55851f21e1329ed03b57dbd6794
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2025-11-06 20:40:52 -0800

    fix #650: Fix GiftExchangePage crashes w/ non-group user and status is not pending (#661)

--- app/gift-exchanges/[id]/page.test.tsx
+++ app/gift-exchanges/[id]/page.test.tsx
@@ -1,26 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import GiftExchangePage from './page';
 import { useAuthContext } from '@/context/AuthContextProvider';
+import * as utils from '@/lib/utils';
+import { TOASTS } from '@/components/Toast/toasts.consts';
 
-const mockGiftExchangeData = {
-  id: '123',
-  name: 'Test Exchange',
-  budget: '50',
-  status: 'pending',
-};
+const routerPush = jest.fn();
 
-const mockMembers = [
-  {
-    id: '1',
-    user_id: 'test-member',
-    member: { avatar: 'https://example.com/mock-avatar.png' },
-  },
-];
+jest.mock('next/navigation', () => ({
+  useParams: () => ({ id: '123' }),
+  useRouter: () => ({
+    push: routerPush,
+  }),
+}));
 
-const mockGiftSuggestions = {
-  match: null,
-  suggestions: [],
-};
+jest.mock('@/context/AuthContextProvider');
 
 jest.mock('@/app/api/openaiConfig/config', () => ({
   openai: {
@@ -32,20 +25,59 @@ jest.mock('@/app/api/openaiConfig/config', () => ({
   },
 }));
 
-jest.mock('next/navigation', () => ({
-  useParams: () => ({ id: '123' }),
+const mockToast = jest.fn();
+
+jest.mock('@/hooks/use-toast', () => ({
+  useToast: jest.fn(() => ({
+    toast: mockToast,
+  })),
 }));
 
-jest.mock('@/context/AuthContextProvider');
+jest.mock('@/lib/utils', () => ({
+  ...jest.requireActual('@/lib/utils'),
+  signInWithGoogle: jest.fn(),
+}));
 
-describe('GiftExchangePage Warning Modal', () => {
-  beforeEach(() => {
-    jest.resetAllMocks();
+const mockGiftExchangeData = {
+  id: '123',
+  name: 'Test Exchange',
+  budget: '50',
+};
 
-    global.fetch = jest
-      .fn()
+const mockGiftSuggestions = {
+  match: null,
+  suggestions: [],
+};
+
+const mockGroupMember = {
+  id: '1',
+  user_id: 'test-member',
+  member: { avatar: 'https://example.com/mock-avatar.png' },
+};
+
+const mockNonGroupMember = { ...mockGroupMember, id: 'not-a-member' };
+
+const mockMembers = [mockGroupMember];
+
+const mockMemberSession = {
+  session: {
+    user: mockGroupMember,
+  },
+};
+
+const mockNonMemberSession = {
+  session: {
+    user: mockNonGroupMember,
+  },
+};
+
+const mockNoSession = { session: null };
+
+describe('GiftExchangePage', () => {
+  const mockDataFetch = (status: string) => {
+    (global.fetch as jest.Mock)
       .mockResolvedValueOnce({
-        json: async () => mockGiftExchangeData,
+        json: async () => ({ ...mockGiftExchangeData, status }),
       })
       .mockResolvedValueOnce({
         json: async () => mockMembers,
@@ -53,16 +85,30 @@ describe('GiftExchangePage Warning Modal', () => {
       .mockResolvedValueOnce({
         json: async () => mockGiftSuggestions,
       });
+  };
+
+  const mockFetchWithoutValidData = () => {
+    (global.fetch as jest.Mock)
+    .mockResolvedValue({
+      json: async () => ({ error: 'Error' }),
+    });
+  };
+
+  const mockRejectedFetch = () => {
+    (global.fetch as jest.Mock).mockRejectedValueOnce({});
+  };
+
+  beforeEach(() => {
+    global.fetch = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('displays the WarningModal with the join button for a logged-in user who is not a member of a pending group', async () => {
-    (useAuthContext as jest.Mock).mockReturnValue({
-      session: {
-        user: {
-          id: 'not-a-member',
-        },
-      },
-    });
+    (useAuthContext as jest.Mock).mockReturnValue(mockNonMemberSession);
+    mockDataFetch('pending');
 
     render(<GiftExchangePage />);
 
@@ -74,7 +120,8 @@ describe('GiftExchangePage Warning Modal', () => {
   });
 
   it('displays the WarningModal with a Google sign-in button for users who are not signed in and not members of a pending group', async () => {
-    (useAuthContext as jest.Mock).mockReturnValue({ session: null });
+    (useAuthContext as jest.Mock).mockReturnValue(mockNoSession);
+    mockDataFetch('pending');
 
     render(<GiftExchangePage />);
 
@@ -83,5 +130,56 @@ describe('GiftExchangePage Warning Modal', () => {
 
     expect(warningModal).toBeInTheDocument();
     expect(googleButton).toBeInTheDocument();
+  });
+
+  it('redirects to user dashboard and displays Bad Link toast if URL is invalid', async () => {
+    (useAuthContext as jest.Mock).mockReturnValue(mockMemberSession);
+    mockFetchWithoutValidData();
+
+    render(<GiftExchangePage />);
+
+    await waitFor(() => {
+      expect(routerPush).toHaveBeenCalledWith('/dashboard');
+
+      expect(mockToast).toHaveBeenCalledWith(TOASTS.badLinkToast);
+    });
+  });
+
+  it('displays a Server Error toast if data fetching throws an error', async () => {
+    (useAuthContext as jest.Mock).mockReturnValue(mockMemberSession);
+    mockRejectedFetch();
+
+    render(<GiftExchangePage />);
+
+    await waitFor(() => {
+      expect(mockToast).toHaveBeenCalledWith(TOASTS.errorToast);
+    });
+  });
+
+  it('redirects to the dashboard and displays an Expired Link toast if user is not a member of a group that has already had its Drawing', async () => {
+    (useAuthContext as jest.Mock).mockReturnValue(mockNonMemberSession);
+    mockDataFetch('active');
+
+    render(<GiftExchangePage />);
+
+    await waitFor(() => {
+      expect(routerPush).toHaveBeenCalledWith('/dashboard');
+
+      expect(mockToast).toHaveBeenCalledWith(TOASTS.expiredLinkToast);
+    });
+  });
+
+  it('calls signInWithGoogle function if the user is not logged in and the group has already had its Drawing', async () => {
+    (useAuthContext as jest.Mock).mockReturnValue(mockNoSession);
+    (utils.signInWithGoogle as jest.Mock).mockResolvedValue(undefined);
+    mockDataFetch('active');
+
+    render(<GiftExchangePage />);
+
+    await waitFor(() => {
+      expect(utils.signInWithGoogle as jest.Mock).toHaveBeenCalledWith({
+        redirectPath: expect.any(String),
+      });
+    });
   });
 });
