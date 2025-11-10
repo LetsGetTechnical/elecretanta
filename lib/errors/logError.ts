6ea7a41cc4c31e69a5051ee80316b85959dbc229
Hector <baseballhector@gmail.com>
2025-11-10 16:32:42 -0500

    fix: improves readability by logging errors as a single object and adjusts log for unknown errors

--- lib/errors/logError.ts
+++ lib/errors/logError.ts
@@ -17,33 +17,72 @@ import {
  */
 export default function logError(error: unknown): NextResponse {
   if (error instanceof SupabaseError) {
-    console.error('Supabase error:', error);
+    console.error({
+      name: error.name,
+      message: error.message,
+      statusCode: error.statusCode,
+      details: error.details,
+      stack: error.stack,
+    });
 
     return NextResponse.json({ error: error.message });
   } else if (error instanceof BackendError) {
-    console.error('Backend error:', error);
+    console.error({
+      name: error.name,
+      message: error.message,
+      statusCode: error.statusCode,
+      details: error.details,
+      stack: error.stack,
+    });
 
     return NextResponse.json(
       { error: error.message },
       { status: error.statusCode },
     );
   } else if (error instanceof OpenAiError) {
-    console.error('OpenAI error:', error);
+    console.error({
+      name: error.name,
+      message: error.message,
+      statusCode: error.statusCode,
+      details: error.details,
+      stack: error.stack,
+    });
 
     return NextResponse.json(
       { error: error.message },
       { status: error.statusCode },
     );
   } else if (error instanceof GoogleError) {
-    console.error('Google error:', error);
+    console.error({
+      name: error.name,
+      message: error.message,
+      statusCode: error.statusCode,
+      details: error.details,
+      stack: error.stack,
+    });
 
     return NextResponse.json(
       { error: error.message },
       { status: error.statusCode },
     );
+  } else if (error instanceof Error) {
+    console.error({
+      name: error.name,
+      message: error.message,
+      stack: error.stack,
+    });
+
+    return NextResponse.json({ error: error.message, status: 500 });
+  } else {
+    console.error({
+      name: 'UnknownError',
+      message: 'An unknown error occurred',
+      details: error,
+    });
+
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 },
+    );
   }
-
-  console.error('Unexpected error:', error);
-
-  return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
 }
