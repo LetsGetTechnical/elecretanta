7212ee1088084c1685c7a57f9f2ba34a904ef586
Alex Appleget <alexappleget2014@gmail.com>
2025-11-18 13:28:51 -0600

    Test: added rendering for test

--- components/GiftExchangeHeader/GiftExchangeHeader.test.tsx
+++ components/GiftExchangeHeader/GiftExchangeHeader.test.tsx
@@ -191,7 +191,16 @@ describe('GiftExchangeHeader owner permissions', () => {
     })
   });
 
-  it('should disable the draw gift exchange button when gift exchange is being drawn', async () => {});
+  it('should disable the draw gift exchange button when gift exchange is being drawn', async () => {
+    
+    render(
+      <GiftExchangeHeader
+      giftExchangeData={mockGiftExchangeDataPending}
+      members={mockMembersData}
+      id={mockGiftExchangeDataPending.id} 
+      />
+    );
+  });
 
   it('should render the complete gift exchange button when user_id and owner_id match', async () => {
     
